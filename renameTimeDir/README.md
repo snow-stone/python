62e851f3654da7bfe5eab24d8fcaa0a519a4fc70
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-04-17 22:05:57 +0200

    2nd version : enable the function to choose interval of interest and calculate the expected uniform spacing time dir names.

--- renameTimeDir/README.md
+++ renameTimeDir/README.md
@@ -1,31 +1,42 @@
 # renameTimeDir.py
 
 ## source
+modified from last version
 
 ## Functionality
-convert every time dir in `processor{0..31}`which has a `.` to the regular format that
-it doesn't end with 0 nor `.`
+for the fact that OF sometimes produces `109.00000000011951` instead of `109` as expected:   
+convert chosed interval of time dirs in `processor{0..31}` to the calculated(expected) time dirs
 
 ## Line
 ```python
-os.rename(path+dir_before[i], path+dir_after[i])
-# converting path+dir_before[i] to path+dir_after[i]
+os.rename(path+dir_before[i], path+dir_calc[i])
+# converting path+dir_before[i] to path+dir_calc[i]
 ```
 
 ## Developpment Approacheseses
+function `formatOpenFOAM(start1,end1,start2,end2,step,path,ifRename)`:   
+start1 and end1 is for indicating the time interval that we are interested in of course the dirs in this case must be sorted numerically (thus `constant` must be removed from `dir_list`).   
+start2 and end2 is for indicating the target time dir names with strictly EQUAL step. 
 
 ## Input
 ### Internal input
-bool flag in `formatOpenFOAM(path, False)` need to be set
+bool flag in `formatOpenFOAM(127.9, 150.4, 127.9, 150.5, 0.045, path, True)` need to be set
 
 ## Output
-renamed time dirs if bool flag in `formatOpenFOAM(path, False)` is set to `True`
+
+renamed time dirs if bool flag in formatOpenFOAM(127.9, 150.4, 127.9, 150.5, 0.045, path, True)` is set to `True`
 
 ## How
 step 0 : set flag to `False` and see what will happen. If everything corresponds to what you are expecting. then follow step 1.   
 step 1 : set flag to `True` and re-run.
 
+## Improvement
+sort function added   
+`constant` dir excluded from consideration   
+move the `if '.' in string` inside `convert2TimeDirName` and add output for Integer outputSteps.
+
 ## Limit
-no sort function introduced, everything inside `processor{0..31}` is treated equally including the `constant` dir.
+need to tune the function `formatOpenFOAM(start1,end1,start2,end2,step,path,ifRename)` to get the right `expected time dirs` manually. Be careful.   
+didn't remove the part concerning `dir_after`.
 
 # Cope with limit
--- renameTimeDir/renameTimeDir.py
+++ renameTimeDir/renameTimeDir.py
@@ -1,14 +1,27 @@
 
 def convert2TimeDirName(string):
-    while string[-1] == '0':
-        string = string[:-1] # remove the trailing '0'
-    while string[-1] == '.':
-        string = string.rstrip('.')
+    if '.' in string:
+        while string[-1] == '0':
+            string = string[:-1] # remove the trailing '0'
+        while string[-1] == '.': # then after that, remove the trailing '.'
+            string = string.rstrip('.')
+    else:
+        print 'Integer timeStep here :', string
     return string
 
+def getInterval(start,end,sorted_dir_list_in):
+    sorted_dir_list_out=[]
+    for dir_I in sorted_dir_list_in:
+        if float(dir_I) < start or float(dir_I) > end :
+            continue
+        else :
+            sorted_dir_list_out.append(dir_I)
+    
+    return sorted_dir_list_out
 
-def formatOpenFOAM(path, ifRename):
+def formatOpenFOAM(start1,end1,start2,end2,step,path,ifRename):
     import os
+    import numpy as np
 #    path = './'
     
 #    name_list = os.listdir(path)
@@ -22,21 +35,42 @@ def formatOpenFOAM(path, ifRename):
 #    dir_list = name_list
 #    print dir_list
     
+#   Very Important here : remove dir named 'constant'
+    dir_list.remove('constant')
+    #print "dir_list is here : ", dir_list
+#    print "sorted dir_list is here : ",sorted(dir_list,key=float)
     dir_before=[]
     dir_after=[]
+    dir_calc=[]
     
-    for dir_I in dir_list:
-        if "." in dir_I:
-            dir_before.append(dir_I)
-            dir_after.append(convert2TimeDirName(dir_I))
+#    print 'getInterval(1.09, 1.18, sorted(dir_list,key=float))'
+#    print getInterval(1.09, 1.18, sorted(dir_list,key=float))
+#    for dir_I in sorted(dir_list,key=float):
+    sortedInterval = getInterval(start1, end1, sorted(dir_list,key=float))
+    #print 'sortedInterval : ', sortedInterval
+    #dir_calc_array = np.linspace(start2, end2, len(sortedInterval))
+    dir_calc_array = np.arange(start2, end2, step)
+    dir_calc = [convert2TimeDirName(str(x)) for x in dir_calc_array]
+    for dir_I in sortedInterval:
+        # only decimals are concerned, if integer, integers are naturally the right format
+##        if "." in dir_I:
+##            dir_before.append(dir_I)
+##            dir_after.append(convert2TimeDirName(dir_I))
+        dir_before.append(dir_I)
+        dir_after.append(convert2TimeDirName(dir_I))
     
 #    print dir_before
 #    print sorted(dir_before,key=float)
+    print 'len(dir_before), len(dir_after), len(dir_calc)'
+    print len(dir_before), len(dir_after), len(dir_calc)
     for i in range(len(dir_before)):
-        info = "converting " + dir_before[i] + " to " + dir_after[i]
-        print info.rjust(50)
+        #info = "converting " + dir_before[i] + " to " + dir_after[i] + "    calculated " + dir_calc[i]
+        info = "converting " + dir_before[i] + " to calculated " + dir_calc[i]
+        # print for right align
+        print info.rjust(100)
         if ifRename :
-            os.rename(path+dir_before[i], path+dir_after[i])
+            #os.rename(path+dir_before[i], path+dir_after[i])
+            os.rename(path+dir_before[i], path+dir_calc[i])
     
     if not ifRename :
         print "Not renamed yet."
@@ -49,9 +83,9 @@ def main():
     relativePath='./'
     pathList = [relativePath+'processor'+str(i)+'/' for i in range(32)]
 #    pathList = relativePath+'processor'+str(0)+'/'
-    print "pathList ", pathList
     for path in pathList:
-        formatOpenFOAM(path, False)
-#    formatOpenFOAM(relativePath, True)
+        print "path = ", path
+        formatOpenFOAM(127.9, 150.4, 127.9, 150.5, 0.045, path, True)
+#    formatOpenFOAM(127.9, 150.4, 127.9, 150.5, 0.045, pathList, False)
 
 main()
