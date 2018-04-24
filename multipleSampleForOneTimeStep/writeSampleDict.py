45ef2a0450f880a11b4d708bcbd712955c92bd23
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-04-24 21:31:16 +0200

    1st modif

--- multipleSampleForOneTimeStep/writeSampleDict.py
+++ multipleSampleForOneTimeStep/writeSampleDict.py
@@ -3,9 +3,6 @@
 import numpy as np
 import os
 
-#for i in range(10):
-#     f.write("This is line %d\r\n" % (i+1))
-
 def write_headerSetting_sampleLines(f):
     f.write("setFormat raw;\r\n")
     f.write("\r\n")
@@ -16,13 +13,13 @@ def write_headerSetting_sampleLines(f):
     f.write("\tU\r\n")
     f.write(");\r\n")
 
-def writeLines(f,zList,N,iList):
-    for i in iList:
+def writeLines(f,z_value_List,N,total_nb_of_Line,resolution):
+    for i in total_nb_of_Line:
         if i in range(N):
             f.write("\t"+"line"+str(i)+"\r\n")
             f.write("\t{\r\n")
             
-            writeStartEnd0(f,zList[i])
+            writeStartEnd0(f,z_value_List[i],resolution)
             
             f.write("\t}\r\n")
             f.write("\r\n")
@@ -30,7 +27,7 @@ def writeLines(f,zList,N,iList):
             f.write("\t"+"line"+str(i)+"\r\n")
             f.write("\t{\r\n")
             
-            writeStartEnd1(f,zList[i-N])
+            writeStartEnd1(f,z_value_List[i-N],resolution)
             
             f.write("\t}\r\n")
             f.write("\r\n")
@@ -38,7 +35,7 @@ def writeLines(f,zList,N,iList):
             f.write("\t"+"line"+str(i)+"\r\n")
             f.write("\t{\r\n")
             
-            writeStartEnd2(f,zList[i-2*N])
+            writeStartEnd2(f,z_value_List[i-2*N],resolution)
             
             f.write("\t}\r\n")
             f.write("\r\n")      
@@ -46,7 +43,7 @@ def writeLines(f,zList,N,iList):
             f.write("\t"+"line"+str(i)+"\r\n")
             f.write("\t{\r\n")
             
-            writeStartEnd3(f,zList[i-3*N])
+            writeStartEnd3(f,z_value_List[i-3*N],resolution)
             
             f.write("\t}\r\n")
             f.write("\r\n")
@@ -55,57 +52,56 @@ def writeLines(f,zList,N,iList):
             print "There is a problem !\n"
             print "###########################\n"
     
-def writeStartEnd0(f,z):
+def writeStartEnd0(f,z,nPoints):
     f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
     f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
     f.write("\t\t"+ "start\t" + "(0. 0 "+ z +");" +"\r\n")
     f.write("\t\t"+ "end\t" + "(0. -0.004 "+ z +");" +"\r\n")
-    f.write("\t\t"+ "nPoints\t" + "200;" +"\r\n")
+    f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
 
-def writeStartEnd1(f,z):
+def writeStartEnd1(f,z,nPoints):
     f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
     f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
     f.write("\t\t"+ "start\t" + "(0. 0 "+ z +");" +"\r\n")
     f.write("\t\t"+ "end\t" + "(0. 0.004 "+ z +");" +"\r\n")
-    f.write("\t\t"+ "nPoints\t" + "200;" +"\r\n")
+    f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
     
-def writeStartEnd2(f,z):
+def writeStartEnd2(f,z,nPoints):
     f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
     f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
     f.write("\t\t"+ "start\t" + "(0 0 "+ z +");" +"\r\n")
     f.write("\t\t"+ "end\t" + "(-0.004 0 "+ z +");" +"\r\n")
-    f.write("\t\t"+ "nPoints\t" + "200;" +"\r\n")
+    f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
 
-def writeStartEnd3(f,z):
+def writeStartEnd3(f,z,nPoints):
     f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
     f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
     f.write("\t\t"+ "start\t" + "(0 0 "+ z +");" +"\r\n")
     f.write("\t\t"+ "end\t" + "(0.004 0 "+ z +");" +"\r\n")
-    f.write("\t\t"+ "nPoints\t" + "200;" +"\r\n")
+    f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
 
-def write_LineSet(f,zList,N,iList):
+def write_LineSet(f,z_value_List,N,total_nb_of_Line,resolution):
     f.write("sets\r\n")
     f.write("(\r\n")
-    writeLines(f,zList,N,iList)
+    writeLines(f,z_value_List,N,total_nb_of_Line,resolution)
     f.write(");\r\n")
 
-def write_sampleLines(fileName,zList,N,iList):
+def write_sampleLines(fileName,z_value_List,N,total_nb_of_Line,resolution):
     f= open(fileName,"a")
     write_headerSetting_sampleLines(f)
-    write_LineSet(f,zList,N,iList)
+    write_LineSet(f,z_value_List,N,total_nb_of_Line,resolution)
     f.close()
     
-def NLines2Dict(N):
-    #l = np.linspace(0,0.04,num=10)
-    l = np.linspace(0.0052,0.0352,num=N)
+def NLines2Dict(Nb_slices_along_z):
+    l = np.linspace(0.0052,0.0352,num=Nb_slices_along_z)
     listStr = [str(x) for x in l]
     print "A list of all z positions :\n"
     print listStr
     
     dictName="../../system/sampleDict_lines"
-    write_sampleLines(dictName,listStr,N,range(4*N))
+    write_sampleLines(dictName,listStr,Nb_slices_along_z,range(4*Nb_slices_along_z),resolution=200)
     
     os.system('dos2unix '+dictName)
 
+# exactly 40 mesh grid in 0.03m
 NLines2Dict(40)
-#write_N_sampleLines2Dict(10)
