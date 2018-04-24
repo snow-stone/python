544722ff03a596801032b089fc1b86fedcd5544e
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-04-24 21:32:42 +0200

    2nd modif : much shorter/nicer coding

--- multipleSampleForOneTimeStep/writeSampleDict.py
+++ multipleSampleForOneTimeStep/writeSampleDict.py
@@ -13,44 +13,84 @@ def write_headerSetting_sampleLines(f):
     f.write("\tU\r\n")
     f.write(");\r\n")
 
+#def writeLines(f,z_value_List,N,total_nb_of_Line,resolution):
+#    for i in total_nb_of_Line:
+#        if i in range(N):
+#            f.write("\t"+"line"+str(i)+"\r\n")
+#            f.write("\t{\r\n")
+#            
+#            writeStartEnd0(f,z_value_List[i],resolution)
+#            
+#            f.write("\t}\r\n")
+#            f.write("\r\n")
+#        elif i >= N and i < 2*N:
+#            f.write("\t"+"line"+str(i)+"\r\n")
+#            f.write("\t{\r\n")
+#            
+#            writeStartEnd1(f,z_value_List[i-N],resolution)
+#            
+#            f.write("\t}\r\n")
+#            f.write("\r\n")
+#        elif i >= 2*N and i < 3*N:
+#            f.write("\t"+"line"+str(i)+"\r\n")
+#            f.write("\t{\r\n")
+#            
+#            writeStartEnd2(f,z_value_List[i-2*N],resolution)
+#            
+#            f.write("\t}\r\n")
+#            f.write("\r\n")      
+#        elif i >= 3*N and i < 4*N:
+#            f.write("\t"+"line"+str(i)+"\r\n")
+#            f.write("\t{\r\n")
+#            
+#            writeStartEnd3(f,z_value_List[i-3*N],resolution)
+#            
+#            f.write("\t}\r\n")
+#            f.write("\r\n")
+#        else:
+#            print "###########################\n"
+#            print "There is a problem !\n"
+#            print "###########################\n"
+
+
 def writeLines(f,z_value_List,N,total_nb_of_Line,resolution):
     for i in total_nb_of_Line:
-        if i in range(N):
-            f.write("\t"+"line"+str(i)+"\r\n")
-            f.write("\t{\r\n")
-            
-            writeStartEnd0(f,z_value_List[i],resolution)
-            
-            f.write("\t}\r\n")
-            f.write("\r\n")
-        elif i >= N and i < 2*N:
-            f.write("\t"+"line"+str(i)+"\r\n")
-            f.write("\t{\r\n")
-            
-            writeStartEnd1(f,z_value_List[i-N],resolution)
-            
-            f.write("\t}\r\n")
-            f.write("\r\n")
-        elif i >= 2*N and i < 3*N:
-            f.write("\t"+"line"+str(i)+"\r\n")
-            f.write("\t{\r\n")
-            
-            writeStartEnd2(f,z_value_List[i-2*N],resolution)
-            
-            f.write("\t}\r\n")
-            f.write("\r\n")      
-        elif i >= 3*N and i < 4*N:
-            f.write("\t"+"line"+str(i)+"\r\n")
-            f.write("\t{\r\n")
-            
-            writeStartEnd3(f,z_value_List[i-3*N],resolution)
-            
-            f.write("\t}\r\n")
-            f.write("\r\n")
-        else:
-            print "###########################\n"
-            print "There is a problem !\n"
-            print "###########################\n"
+        whichLine = i // N
+        j = i % N
+        
+        f.write("\t"+"line"+str(i)+"\r\n")
+        f.write("\t{\r\n")
+        writeStartEnd(whichLine,f,z_value_List[j],resolution)
+        f.write("\t}\r\n")
+        f.write("\r\n")
+
+def writeStartEnd(whichLine,f,z,nPoints):
+    if whichLine == 0:
+        f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
+        f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
+        f.write("\t\t"+ "start\t" + "(0. 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "end\t" + "(0. -0.004 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
+    elif whichLine == 1:
+        f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
+        f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
+        f.write("\t\t"+ "start\t" + "(0. 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "end\t" + "(0. 0.004 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
+    elif whichLine == 2:
+        f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
+        f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
+        f.write("\t\t"+ "start\t" + "(0 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "end\t" + "(-0.004 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
+    elif whichLine == 3:
+        f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
+        f.write("\t\t"+ "axis\t" + "distance;" +"\r\n")
+        f.write("\t\t"+ "start\t" + "(0 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "end\t" + "(0.004 0 "+ z +");" +"\r\n")
+        f.write("\t\t"+ "nPoints\t" + str(nPoints) + ";" +"\r\n")
+    else:
+        print "We've got a problem here !"        
     
 def writeStartEnd0(f,z,nPoints):
     f.write("\t\t"+ "type\t" + "uniform;" +"\r\n")
