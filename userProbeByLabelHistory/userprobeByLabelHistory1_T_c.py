e667a29455241a35cfde1f922d48f8ec728a216f
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-12-20 14:56:28 +0100

    make cases continuous

--- userProbeByLabelHistory/userprobeByLabelHistory1_T_c.py
+++ userProbeByLabelHistory/userprobeByLabelHistory1_T_c.py
@@ -27,7 +27,6 @@ def userProbeByLabel(ax, caseName, RelativeDataFile, sample, positionSet, cut=0.
     
 def plotField(field):
     positionList = [0,16,32,48]
-    #   sampling = [0,1 ,2 ,3]
     samples = [0,1,2,3]
     arrayNames=["U_x", "U_y", "U_z", "T"]
     arrayName =arrayNames[field]
@@ -38,94 +37,81 @@ def plotField(field):
     latexRMSName = latexRMSNames[field]
     latexMEANNames = [r"$<U_x>$",r"$<U_y>$",r"$<U_z>$",r"$<T>$"]
     latexMEANName = latexMEANNames[field]
-    debit_medium = [
+    cases = [
              "BirdCarreau"+"/"+"inlet_0p5",
-             "Newtonian"+"/"+"Re4000"
-              ]
-    debit_min = [
+             "Newtonian"+"/"+"Re4000",
              "BirdCarreau"+"/"+"inlet_0p3",
              "Newtonian"+"/"+"Re2400"]
-#    color_medium = [
-#             "red",
-#             "blue"              
-#              ]
-#    color_min = [
-#             "red",
-#             "blue"              
-#              ]
-    linestyle_medium = [
+    
+    linestyleList = [
+             "-",
+             "--",
              "-",
              "--"
               ]
-    linestyle_min = [
-             "-",
-             "--"
-              ]
-    marker_medium = [
+    markerList = [
+             "s",
+             "^",
              "s",
              "^" 
               ]
-    marker_min = [
-             "s",
-             "^" 
-              ]
-#    cuts_medium = [
-#            0.5,
-#            0.5
-#            ]
-#    cuts_min = [
-#            0.5,
-#            0.5
-#            ]
     
     plt.style.use('seaborn-white') # from defaut
 
-    mean_medium = np.zeros((len(positionList),2))
-    mean_min = np.zeros((len(positionList),2))
-    std_medium = np.zeros((len(positionList),2))
-    std_min = np.zeros((len(positionList),2))
+    mean = np.zeros((len(positionList),4))
+    std = np.zeros((len(positionList),4))
     
     for i, sample in enumerate(samples):
         fig = plt.figure()
         
         # debit_medium
-        ax1 = fig.add_subplot(2,1,1)
-        for j, case in enumerate(debit_medium):
+        ax1 = fig.add_subplot(4,1,1)
+        ax2 = fig.add_subplot(4,1,2)
+        ax3 = fig.add_subplot(4,1,3)
+        ax4 = fig.add_subplot(4,1,4)
+        for j, case in enumerate(cases):
             if j == 0:
                 if i >= 7:
-                    std_medium[i,j], mean_medium[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.6)
+                    std[i,j], mean[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.6)
                 else :
-                    std_medium[i,j], mean_medium[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.5)
-            else:
+                    std[i,j], mean[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.5)
+            elif j == 1:
                 if i >= 14:
-                    std_medium[i,j], mean_medium[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.5)
+                    std[i,j], mean[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.5)
                 else :
-                    std_medium[i,j], mean_medium[i,j] = userProbeByLabel(ax1, case, dataFile, sample, positionList, cut=0.5)                
-        
-        ax1.legend(bbox_to_anchor=(1.7, 1), ncol=1, shadow=True)
+                    std[i,j], mean[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.5)                
+            elif j == 2:
+                if i >= 14:
+                    std[i,j], mean[i,j] = userProbeByLabel(ax3, case, dataFile, sample, positionList, cut=0.8)
+                else:
+                    std[i,j], mean[i,j] = userProbeByLabel(ax3, case, dataFile, sample, positionList, cut=0.5)
+            elif j ==3 :
+                if i >= 14:
+                    std[i,j], mean[i,j] = userProbeByLabel(ax4, case, dataFile, sample, positionList, cut=0.8)
+                else:
+                    std[i,j], mean[i,j] = userProbeByLabel(ax4, case, dataFile, sample, positionList, cut=0.5)
+            else :
+                print "There's a big problem"
+                        
         ax1.set_xlabel(r"$t$")
         ax1.set_ylabel(latexName)
         ax1.set_ylim(-0.2,1.2)
         ax1.set_xlim(0,0.8)
-        
-        # debit_min
-        ax2 = fig.add_subplot(2,1,2)
-        for j, case in enumerate(debit_min):
-            if j == 0:
-                if i >= 14:
-                    std_min[i,j], mean_min[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.8)
-                else:
-                    std_min[i,j], mean_min[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.5)
-            else:
-                if i >= 14:
-                    std_min[i,j], mean_min[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.8)
-                else:
-                    std_min[i,j], mean_min[i,j] = userProbeByLabel(ax2, case, dataFile, sample, positionList, cut=0.5)
-        ax2.legend(bbox_to_anchor=(1.7, 1), ncol=1, shadow=True)
+
         ax2.set_xlabel(r"$t$")
         ax2.set_ylabel(latexName)
         ax2.set_ylim(-0.2,1.2)
         ax2.set_xlim(0,0.8)
+
+        ax3.set_xlabel(r"$t$")
+        ax3.set_ylabel(latexName)
+        ax3.set_ylim(-0.2,1.2)
+        ax3.set_xlim(0,0.8)
+
+        ax4.set_xlabel(r"$t$")
+        ax4.set_ylabel(latexName)
+        ax4.set_ylim(-0.2,1.2)
+        ax4.set_xlim(0,0.8)        
         
         fig.savefig("../PICTURE_history_c/"+arrayName+"/x_Eq_"+str(positionList[sample]/8.0).replace('.','p')+"D.png", bbox_inches='tight',dpi=300) # bbox_inches = 'tight' is neccessary
 #==============================================================================
@@ -135,37 +121,23 @@ def plotField(field):
     positionList=[x/8.0 for x in positionList]
 
     ax1 = fig_rms.add_subplot(1,1,1)
-    for i, case in enumerate(debit_medium):
-        ax1.plot(positionList,std_medium[:,i],label=debit_medium[i],linestyle=linestyle_medium[i],marker=marker_medium[i])
-#    ax1.legend(bbox_to_anchor=(1.7, 1), ncol=1, shadow=True)
-#    ax1.set_xlabel(r"$x/D$")
-#    ax1.set_ylabel(latexRMSName)
-    
-#    ax2 = fig_rms.add_subplot(2,1,2)
-    for i, case in enumerate(debit_min):
-        ax1.plot(positionList,std_min[:,i],label=debit_min[i],linestyle=linestyle_min[i],marker=marker_medium[i])
+
+    for i, case in enumerate(cases):
+        ax1.plot(positionList,std[:,i],label=cases[i],linestyle=linestyleList[i],marker=markerList[i])
     ax1.legend(bbox_to_anchor=(1., 1), ncol=1, shadow=True)
     ax1.set_xlabel(r"$x/D$")
     ax1.set_ylabel(latexRMSName)
-#    ax1.set_ylim(0,0.4)
     
     fig_rms.savefig('../PICTURE_history_c/'+arrayName+'/RMS_xByD_oneFig.png', bbox_inches='tight',  dpi=300)
 #==============================================================================
 #   Mean
 #==============================================================================
     fig_rms = plt.figure()
-#    positionList=[x/8.0 for x in positionList]
 
     ax1 = fig_rms.add_subplot(1,1,1)
-    for i, case in enumerate(debit_medium):
-        ax1.plot(positionList,mean_medium[:,i],label=debit_medium[i],linestyle=linestyle_medium[i],marker=marker_min[i])
-#    ax1.legend(bbox_to_anchor=(1.7, 1), ncol=1, shadow=True)
-#    ax1.set_xlabel(r"$x/D$")
-#    ax1.set_ylabel(latexMEANName)
-    
-#    ax2 = fig_rms.add_subplot(2,1,2)
-    for i, case in enumerate(debit_min):
-        ax1.plot(positionList,mean_min[:,i],label=debit_min[i],linestyle=linestyle_min[i],marker=marker_min[i])
+
+    for i, case in enumerate(cases):
+        ax1.plot(positionList,mean[:,i],label=cases[i],linestyle=linestyleList[i],marker=markerList[i])
     ax1.legend(bbox_to_anchor=(1., 1), ncol=1, shadow=True)
     ax1.set_xlabel(r"$x/D$")
     ax1.set_ylabel(latexMEANName)
@@ -173,7 +145,6 @@ def plotField(field):
     fig_rms.savefig('../PICTURE_history_c/'+arrayName+'/Mean_xByD_oneFig.png', bbox_inches='tight', dpi=300)
 
 def main():
-#    plotField(field=3)
     for i in range (0,4):
         plotField(field=i)
 
