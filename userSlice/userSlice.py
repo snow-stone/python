7aabd93d8bda0d4f23352473a20be79c12cddf96
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-09 19:51:53 +0100

    add aliasDict for alias naming for simulations

--- userSlice/userSlice.py
+++ userSlice/userSlice.py
@@ -43,7 +43,7 @@ def plotSlice(ax, sliceNumber, path2Data, dataDir, cut=0.5):
     
     return np.mean(data[cutSliceIndex:,2])
     
-def plotCaseWithSlices(ax_cases, path2Data, dataDir, positionList, marker, cut):
+def plotCaseWithSlices(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
     meanOfRMS = np.zeros(len(positionList))
     
     fig, ax_in_case = plt.subplots()
@@ -51,7 +51,7 @@ def plotCaseWithSlices(ax_cases, path2Data, dataDir, positionList, marker, cut):
         meanOfRMS[i] = plotSlice(ax_in_case, position, path2Data, dataDir, cut)
 
     positionList = np.asarray(positionList)
-    ax_cases.plot(positionList/8.0,meanOfRMS, label=dataDir, marker=marker)
+    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
     
 def main():
     plt.style.use('seaborn-white')
@@ -59,10 +59,14 @@ def main():
 #              "Newtonian/Re4000",
 #              "BirdCarreau/inlet_0p3",
 #              "Newtonian/Re2400"]
-    caseList=["BirdCarreau/inlet_0p5",
-              "BirdCarreau/inlet0p5_impinging",
-              "Newtonian/Re4000",
-              "Newtonian/Re4000_impinging"]
+#    caseList=["BirdCarreau/inlet_0p5",
+#              "BirdCarreau/inlet0p5_impinging",
+#              "Newtonian/Re4000",
+#              "Newtonian/Re4000_impinging"]
+    caseList=["BirdCarreau/inlet_0p3",
+             "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  
+		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5"
+             ]
 
     positionList = [0,1,2,3,4,5,6,7,8,9,10,11,12,16,24,32,40,48,56,64,72,73,74,75]
     path2Data = "/home/hluo/Downloads/tmp/shape_square/2a_3_T/python_postProcessing"
@@ -73,29 +77,40 @@ def main():
                 "^"
                 ]
     
+    aliasDict={
+        "BirdCarreau/inlet_0p5":"case1a",
+        "Newtonian/Re4000"     :"case1b",
+        "BirdCarreau/inlet_0p3":"case2a",
+        "Newtonian/Re2400"     :"case2b",
+        "BirdCarreau/inlet_0p3-a_0p5-setT_St_1":"case2c",
+        "BirdCarreau/inlet_0p3-a_0p5-setT_St_5":"case2d",
+        "BirdCarreau/inlet0p5_impinging"       :"case1ai",
+        "Newtonian/Re4000_impinging"           :"case1bi"
+    }
+    
     fig, ax_principle = plt.subplots()
 
     for i, caseDir in enumerate(caseList):
-        plotCaseWithSlices(ax_principle, path2Data, caseDir, positionList, markerList[i], cut=0.7)
+        plotCaseWithSlices(ax_principle, path2Data, caseDir, positionList, markerList[i], aliasDict, cut=0.7)
         
     
     ax_principle.set_xlabel(r"$x/D$")
-    ax_principle.set_ylabel(r"mixing factor")
+    ax_principle.set_ylabel(r"$mixing \quad factor$")
     ax_principle.set_ylim(0,1)
 
-    x_XG0, y_XG0 = dai_debitMin('XG')
-    ax_principle.plot(x_XG0,y_XG0,label='Dai/inlet_0p3',linestyle='--',marker='s')    
-    x_water0, y_water0 = dai_debitMin('EAU')    
-    ax_principle.plot(x_water0,y_water0,label='Dai/Re2400',linestyle='--',marker='^')
-    
-    x_XG1, y_XG1 = dai_debitMoyen('XG')
-    ax_principle.plot(x_XG1,y_XG1,label='Dai/inlet_0p5',linestyle='--',marker='s')    
-    x_water1, y_water1 = dai_debitMoyen('EAU')    
-    ax_principle.plot(x_water1,y_water1,label='Dai/Re4000',linestyle='--',marker='^')
+#    x_XG0, y_XG0 = dai_debitMin('XG')
+#    ax_principle.plot(x_XG0,y_XG0,label='Dai/inlet_0p3',linestyle='--',marker='s')    
+#    x_water0, y_water0 = dai_debitMin('EAU')    
+#    ax_principle.plot(x_water0,y_water0,label='Dai/Re2400',linestyle='--',marker='^')
+#    
+#    x_XG1, y_XG1 = dai_debitMoyen('XG')
+#    ax_principle.plot(x_XG1,y_XG1,label='Dai/inlet_0p5',linestyle='--',marker='s')    
+#    x_water1, y_water1 = dai_debitMoyen('EAU')    
+#    ax_principle.plot(x_water1,y_water1,label='Dai/Re4000',linestyle='--',marker='^')
 
 
     ax_principle.legend(bbox_to_anchor=(1.4, 1), ncol=1, shadow=True)
-    ax_principle.set_xlim(0,40)
+#    ax_principle.set_xlim(0,40)
     
     fig.savefig(path2Data+"/"+'PICTURE_mixingFactor/mixingFactor.png', bbox_inches='tight', dpi=300)
     
