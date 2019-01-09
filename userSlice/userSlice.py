f3c048ea88cf5dbdc378adc19f26642df3fa4d01
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-09 20:11:51 +0100

    add aliasDict_Dai and change order to suit simu with exp

--- userSlice/userSlice.py
+++ userSlice/userSlice.py
@@ -55,26 +55,27 @@ def plotCaseWithSlices(ax_cases, path2Data, dataDir, positionList, marker, alias
     
 def main():
     plt.style.use('seaborn-white')
-#    caseList=["BirdCarreau/inlet_0p5",
-#              "Newtonian/Re4000",
-#              "BirdCarreau/inlet_0p3",
-#              "Newtonian/Re2400"]
+    caseList=["BirdCarreau/inlet_0p5",
+              "Newtonian/Re4000",
+              "BirdCarreau/inlet_0p3",
+              "Newtonian/Re2400"]
 #    caseList=["BirdCarreau/inlet_0p5",
 #              "BirdCarreau/inlet0p5_impinging",
 #              "Newtonian/Re4000",
 #              "Newtonian/Re4000_impinging"]
-    caseList=["BirdCarreau/inlet_0p3",
-             "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  
-		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5"
-             ]
+    #forcing effect
+#    caseList=["BirdCarreau/inlet_0p3",
+#             "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  
+#		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5"
+#             ]
 
     positionList = [0,1,2,3,4,5,6,7,8,9,10,11,12,16,24,32,40,48,56,64,72,73,74,75]
     path2Data = "/home/hluo/Downloads/tmp/shape_square/2a_3_T/python_postProcessing"
     
     markerList=["s",
                 "^",
-                "s",
-                "^"
+                "d",
+                "v"
                 ]
     
     aliasDict={
@@ -88,6 +89,13 @@ def main():
         "Newtonian/Re4000_impinging"           :"case1bi"
     }
     
+    aliasDict_Dai={
+        'Dai/inlet_0p5' : "exp1a",
+        'Dai/Re4000'    : "exp1b",
+        'Dai/inlet_0p3' : "exp2a",
+        'Dai/Re2400'    : "exp2b"
+    }
+    
     fig, ax_principle = plt.subplots()
 
     for i, caseDir in enumerate(caseList):
@@ -98,18 +106,18 @@ def main():
     ax_principle.set_ylabel(r"$mixing \quad factor$")
     ax_principle.set_ylim(0,1)
 
-#    x_XG0, y_XG0 = dai_debitMin('XG')
-#    ax_principle.plot(x_XG0,y_XG0,label='Dai/inlet_0p3',linestyle='--',marker='s')    
-#    x_water0, y_water0 = dai_debitMin('EAU')    
-#    ax_principle.plot(x_water0,y_water0,label='Dai/Re2400',linestyle='--',marker='^')
-#    
-#    x_XG1, y_XG1 = dai_debitMoyen('XG')
-#    ax_principle.plot(x_XG1,y_XG1,label='Dai/inlet_0p5',linestyle='--',marker='s')    
-#    x_water1, y_water1 = dai_debitMoyen('EAU')    
-#    ax_principle.plot(x_water1,y_water1,label='Dai/Re4000',linestyle='--',marker='^')
+    x_XG1, y_XG1 = dai_debitMoyen('XG')
+    ax_principle.plot(x_XG1,y_XG1,label=aliasDict_Dai['Dai/inlet_0p5'],linestyle='--',marker='s',fillstyle='none')    
+    x_water1, y_water1 = dai_debitMoyen('EAU')    
+    ax_principle.plot(x_water1,y_water1,label=aliasDict_Dai['Dai/Re4000'],linestyle='--',marker='^',fillstyle='none')
+
+    x_XG0, y_XG0 = dai_debitMin('XG')
+    ax_principle.plot(x_XG0,y_XG0,label=aliasDict_Dai['Dai/inlet_0p3'],linestyle='--',marker='d',fillstyle='none')    
+    x_water0, y_water0 = dai_debitMin('EAU')    
+    ax_principle.plot(x_water0,y_water0,label=aliasDict_Dai['Dai/Re2400'],linestyle='--',marker='v',fillstyle='none')
 
 
-    ax_principle.legend(bbox_to_anchor=(1.4, 1), ncol=1, shadow=True)
+    ax_principle.legend(bbox_to_anchor=(1, 1), ncol=2, shadow=True)
 #    ax_principle.set_xlim(0,40)
     
     fig.savefig(path2Data+"/"+'PICTURE_mixingFactor/mixingFactor.png', bbox_inches='tight', dpi=300)
