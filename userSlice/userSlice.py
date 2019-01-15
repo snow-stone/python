f4f2108771d038d2d9112fbc964d0179f834651f
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-15 14:52:44 +0100

    add multiple version plotCaseWithSlices and plotSlice

--- userSlice/userSlice.py
+++ userSlice/userSlice.py
@@ -55,14 +55,46 @@ def plotSlice_Dai(ax, sliceNumber, path2Data, dataDir, cut=0.5):
     ax.set_title(dataDir)
     
     return np.mean(data[cutSliceIndex:,2])
+
+def plotSlice_Dai_0p8(ax, sliceNumber, path2Data, dataDir, cut=0.5):
+    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms_Dai_0p8")
     
-def plotCaseWithSlices(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+    time = data[:,0]
+    cutSliceIndex = int(cut*len(time))
+    
+    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
+    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
+    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
+    ax.set_title(dataDir)
+    
+    return np.mean(data[cutSliceIndex:,2])
+
+def plotCaseWithSlices_old(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
     meanOfRMS = np.zeros(len(positionList))
     
     fig, ax_in_case = plt.subplots()
     for i, position in enumerate(positionList):
-#        meanOfRMS[i] = plotSlice_Dai(ax_in_case, position, path2Data, dataDir, cut)
         meanOfRMS[i] = plotSlice_old(ax_in_case, position, path2Data, dataDir, cut)
+
+    positionList = np.asarray(positionList)
+    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
+    
+def plotCaseWithSlices_Dai(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+    meanOfRMS = np.zeros(len(positionList))
+    
+    fig, ax_in_case = plt.subplots()
+    for i, position in enumerate(positionList):
+        meanOfRMS[i] = plotSlice_Dai(ax_in_case, position, path2Data, dataDir, cut)
+
+    positionList = np.asarray(positionList)
+    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
+    
+def plotCaseWithSlices_Dai_0p8(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+    meanOfRMS = np.zeros(len(positionList))
+    
+    fig, ax_in_case = plt.subplots()
+    for i, position in enumerate(positionList):
+        meanOfRMS[i] = plotSlice_Dai_0p8(ax_in_case, position, path2Data, dataDir, cut)
 
     positionList = np.asarray(positionList)
     ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
@@ -74,15 +106,15 @@ def main():
 #              "BirdCarreau/inlet_0p3",
 #              "Newtonian/Re2400"]
     #impinging
-#    caseList=["BirdCarreau/inlet_0p5",
-#              "BirdCarreau/inlet0p5_impinging",
-#              "Newtonian/Re4000",
-#              "Newtonian/Re4000_impinging"]
+    caseList=["BirdCarreau/inlet_0p5",
+              "BirdCarreau/inlet0p5_impinging",
+              "Newtonian/Re4000",
+              "Newtonian/Re4000_impinging"]
     #forcing effect
-    caseList=["BirdCarreau/inlet_0p3",
-             "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  
-		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5"
-             ]
+#    caseList=["BirdCarreau/inlet_0p3",
+#             "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  
+#		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5"
+#             ]
 
 #    positionList = [0,1,2,3,4,5,6,7,8,9,10,11,12,16,24,32,40,48,56,64,72,73,74,75]
     positionList = [1,2,3,4,5,6,7,8,9,10,11,12,16,24,32,40,48,56,64,72,73,74,75]
@@ -115,12 +147,16 @@ def main():
     fig, ax_principle = plt.subplots()
 
     for i, caseDir in enumerate(caseList):
-        plotCaseWithSlices(ax_principle, path2Data, caseDir, positionList, markerList[i], aliasDict, cut=0.7)
+        plotCaseWithSlices_old(ax_principle, path2Data, caseDir, positionList, markerList[i], aliasDict, cut=0.7)
+#        if i==1 or i==3:
+#            plotCaseWithSlices_Dai_0p8(ax_principle, path2Data, caseDir, positionList, markerList[i], aliasDict, cut=0.7)
+#        else:
+#            plotCaseWithSlices_Dai(ax_principle, path2Data, caseDir, positionList, markerList[i], aliasDict, cut=0.7)
         
     
     ax_principle.set_xlabel(r"$x/D$")
     ax_principle.set_ylabel(r"$mixing \quad factor$")
-    ax_principle.set_ylim(0,1)
+#    ax_principle.set_ylim(0,1.0)
 
 #    x_XG1, y_XG1 = dai_debitMoyen('XG')
 #    ax_principle.plot(x_XG1,y_XG1,label=aliasDict_Dai['Dai/inlet_0p5'],linestyle='-',marker='s',fillstyle='none')    
