f616d9aa833375b4351a2cecaf279fe1995947a8
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-16 21:03:02 +0100

    making module

--- userCropFigures/userCropFigures.py
+++ userCropFigures/userCropFigures.py
@@ -1,11 +1,19 @@
 
 from PIL import Image
-
-img = Image.open("example.png")
-
-coords = (887, 129, 1146, 426)
-
-img.show()
-cropped = img.crop(coords)
-
-cropped.show()
+ 
+def crop(image_path, coords, saved_location):
+    """
+    @param image_path: The path to the image to edit
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path to save the cropped image
+    """
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(saved_location)
+    cropped_image.show()
+ 
+ 
+if __name__ == '__main__':
+    image = 'example.png'
+    Image.open(image).show()
+    crop(image, (887, 129, 1146, 426), 'cropped.png')
