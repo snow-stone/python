d9fc1d4321cfd92b4a87f973db5821e3eee28f41
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-16 21:16:31 +0100

    test passed : for loop

--- userCropFigures/userCropFigures.py
+++ userCropFigures/userCropFigures.py
@@ -12,12 +12,21 @@ def crop(image_path, coords, saved_location):
     cropped_image.save(saved_location)
     cropped_image.show()
  
- 
 if __name__ == '__main__':
 #    image = 'example.png'
 #    Image.open(image).show()
     dataBase = '/store/8simu_tmp/shape_square/2a_3_T'
-    relativePath = 'BirdCarreau/inlet_0p5'
+    paths = [
+            'BirdCarreau/inlet_0p5',
+            'Newtonian/Re4000',
+            'BirdCarreau/inlet0p5_impinging',
+            'Newtonian/Re4000_impinging',
+            'BirdCarreau/inlet_0p3',
+            'Newtonian/Re2400',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_1',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_5'
+           ]
     imageName = 'k_mean_nonD'
-    image = dataBase + '/' + relativePath + '/' + imageName + '.png'
-    crop(image, (480, 567, 2277, 1281), dataBase+'/'+imageName+'_cropped.png')
+    for i, path in enumerate(paths):
+        image = dataBase + '/' + path + '/' + imageName + '.png'
+        crop(image, (480, 567, 2277, 1281), dataBase+'/'+imageName+'_cropped.png')
