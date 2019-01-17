bfa90b0f8449bd55ef2aa522b6b1198b02145e67
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-17 10:04:57 +0100

    add global font control

--- userPressureDrop/userPressureDrop.py
+++ userPressureDrop/userPressureDrop.py
@@ -30,6 +30,8 @@ def plotCase(path2Data,dataDir,cut):
 
 def main():
     plt.style.use('seaborn-white')
+    plt.rcParams['font.size'] = 10 # defaut value is 10
+    plt.rcParams['savefig.dpi'] = 100
     caseList=["BirdCarreau/inlet_0p5",
               "Newtonian/Re4000",
               "BirdCarreau/inlet_0p3",
@@ -68,7 +70,7 @@ def main():
     x = np.arange(len(labelTuple))
     plt.bar(x+0.5, height = pressureDropList, width=0.4, color=colorList)
     plt.xticks(x+0.75, labelTuple) # xticks can only chagned by plt object. axes don't work !!
-    plt.xlim(0,7)
+    plt.xlim(0,6.5)
     plt.ylabel(r'$\Delta p/\rho \quad (m^2/s^2)$')
     plt.savefig(path2Data+"/"+'pressureDrop/pressureDrop.png', bbox_inches='tight', dpi=300)
     
