f8ac4e9f3853cc690a3e846f5763b7d83c332c4d
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-21 12:10:10 +0100

    comment out useless functions

--- userSlice/userSlice_mean_and_rms.py
+++ userSlice/userSlice_mean_and_rms.py
@@ -30,19 +30,19 @@ def dai_debitMoyen(fluid):
 
     return data[:,0], data[:,1]
 
-def plotSlice_old1(ax, sliceNumber, path2Data, dataDir, cut=0.5):
-    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms")
-    
-    time = data[:,0]
-    cutSliceIndex = int(cut*len(time))
-    
-    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
-    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
-    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
-    ax.set_title(dataDir)
-    
-    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
-
+#def plotSlice_old1(ax, sliceNumber, path2Data, dataDir, cut=0.5):
+#    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms")
+#    
+#    time = data[:,0]
+#    cutSliceIndex = int(cut*len(time))
+#    
+#    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
+#    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
+#    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
+#    ax.set_title(dataDir)
+#    
+#    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
+#
 def plotSlice_old(ax, sliceNumber, path2Data, dataDir, cut=0.5, ifPlotInter=True):
     data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms")
     
@@ -60,52 +60,52 @@ def plotSlice_old(ax, sliceNumber, path2Data, dataDir, cut=0.5, ifPlotInter=True
         print "====================="
     
     return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
-
-def plotSlice_Dai(ax, sliceNumber, path2Data, dataDir, cut=0.5):
-    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms_Dai")
-
-    time = data[:,0]
-    cutSliceIndex = int(cut*len(time))
-    
-    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
-    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
-    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
-    ax.set_title(dataDir)
-
-    print "for slice number : ", sliceNumber
-    print "mean : ", np.mean(data[cutSliceIndex:,1])
-    
-    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
-
-def plotSlice_Dai_0p8(ax, sliceNumber, path2Data, dataDir, cut=0.5):
-    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms_Dai_0p8")
-    
-    time = data[:,0]
-    cutSliceIndex = int(cut*len(time))
-    
-    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
-    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
-    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
-    ax.set_title(dataDir)
-    
-    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
-
-def plotCaseWithSlices_old1(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
-    meanOfMEAN = np.zeros(len(positionList))
-    meanOfRMS = np.zeros(len(positionList))
-    
-    fig, ax_in_case = plt.subplots()
-    for i, position in enumerate(positionList):
-#        meanOfRMS[i] = plotSlice_old(ax_in_case, position, path2Data, dataDir, cut)
-        meanOfMEAN[i], meanOfRMS[i] = plotSlice_old1(ax_in_case, position, path2Data, dataDir, cut)
-
-    print aliasDict[dataDir]+" :"
-    print "mean : "
-    print meanOfMEAN
-    print "rms : "
-    print meanOfRMS
-    positionList = np.asarray(positionList)
-    ax_cases.plot(positionList/8.0, meanOfRMS/meanOfMEAN, label=aliasDict[dataDir], marker=marker)
+#
+#def plotSlice_Dai(ax, sliceNumber, path2Data, dataDir, cut=0.5):
+#    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms_Dai")
+#
+#    time = data[:,0]
+#    cutSliceIndex = int(cut*len(time))
+#    
+#    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
+#    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
+#    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
+#    ax.set_title(dataDir)
+#
+#    print "for slice number : ", sliceNumber
+#    print "mean : ", np.mean(data[cutSliceIndex:,1])
+#    
+#    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
+#
+#def plotSlice_Dai_0p8(ax, sliceNumber, path2Data, dataDir, cut=0.5):
+#    data = np.genfromtxt(path2Data+"/"+dataDir+"/"+"userDefinedLog/slice"+str(sliceNumber)+"_mean_rms_Dai_0p8")
+#    
+#    time = data[:,0]
+#    cutSliceIndex = int(cut*len(time))
+#    
+#    p = ax.plot(time[cutSliceIndex:],data[cutSliceIndex:,2])
+#    ax.plot(time[:cutSliceIndex],data[:cutSliceIndex,2],linestyle=':',color=p[0].get_color())
+#    ax.legend(bbox_to_anchor=(1.3, 1), ncol=1, shadow=True)
+#    ax.set_title(dataDir)
+#    
+#    return np.mean(data[cutSliceIndex:,1]),np.mean(data[cutSliceIndex:,2])
+#
+#def plotCaseWithSlices_old1(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+#    meanOfMEAN = np.zeros(len(positionList))
+#    meanOfRMS = np.zeros(len(positionList))
+#    
+#    fig, ax_in_case = plt.subplots()
+#    for i, position in enumerate(positionList):
+##        meanOfRMS[i] = plotSlice_old(ax_in_case, position, path2Data, dataDir, cut)
+#        meanOfMEAN[i], meanOfRMS[i] = plotSlice_old1(ax_in_case, position, path2Data, dataDir, cut)
+#
+#    print aliasDict[dataDir]+" :"
+#    print "mean : "
+#    print meanOfMEAN
+#    print "rms : "
+#    print meanOfRMS
+#    positionList = np.asarray(positionList)
+#    ax_cases.plot(positionList/8.0, meanOfRMS/meanOfMEAN, label=aliasDict[dataDir], marker=marker)
 
 def plotCaseWithSlices_old(ax_cases, path2Data, dataDir, positionList, aliasDict, markerDict, colorDict, cut, ifPlotInter):
     meanOfMEAN = np.zeros(len(positionList))
@@ -146,33 +146,33 @@ def plotCaseWithSlices_old_MI(ax_cases, path2Data, dataDir, positionList, marker
     positionList = np.asarray(positionList)
     ax_cases.plot(positionList/8.0, meanOfRMS/meanOfRMS[0], label=aliasDict[dataDir], marker=marker)
     
-def plotCaseWithSlices_Dai(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
-    meanOfMEAN = np.zeros(len(positionList))
-    meanOfRMS = np.zeros(len(positionList))
-    
-    fig, ax_in_case = plt.subplots()
-    for i, position in enumerate(positionList):
-        meanOfMEAN[i], meanOfRMS[i] = plotSlice_Dai(ax_in_case, position, path2Data, dataDir, cut)
-
-    print aliasDict[dataDir]+" :"
-    print "mean : "
-    print meanOfMEAN
-    print "rms : "
-    print meanOfRMS
-    positionList = np.asarray(positionList)
+#def plotCaseWithSlices_Dai(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+#    meanOfMEAN = np.zeros(len(positionList))
+#    meanOfRMS = np.zeros(len(positionList))
+#    
+#    fig, ax_in_case = plt.subplots()
+#    for i, position in enumerate(positionList):
+#        meanOfMEAN[i], meanOfRMS[i] = plotSlice_Dai(ax_in_case, position, path2Data, dataDir, cut)
+#
+#    print aliasDict[dataDir]+" :"
+#    print "mean : "
+#    print meanOfMEAN
+#    print "rms : "
+#    print meanOfRMS
+#    positionList = np.asarray(positionList)
+##    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
+#    ax_cases.errorbar(positionList/8.0, meanOfMEAN, yerr=meanOfRMS, label=aliasDict[dataDir], marker=marker)
+#    
+#def plotCaseWithSlices_Dai_0p8(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
+#    meanOfMEAN = np.zeros(len(positionList))
+#    meanOfRMS = np.zeros(len(positionList))
+#    
+#    fig, ax_in_case = plt.subplots()
+#    for i, position in enumerate(positionList):
+#        meanOfMEAN[i], meanOfRMS[i] = plotSlice_Dai_0p8(ax_in_case, position, path2Data, dataDir, cut)
+#
+#    positionList = np.asarray(positionList)
 #    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
-    ax_cases.errorbar(positionList/8.0, meanOfMEAN, yerr=meanOfRMS, label=aliasDict[dataDir], marker=marker)
-    
-def plotCaseWithSlices_Dai_0p8(ax_cases, path2Data, dataDir, positionList, marker, aliasDict, cut):
-    meanOfMEAN = np.zeros(len(positionList))
-    meanOfRMS = np.zeros(len(positionList))
-    
-    fig, ax_in_case = plt.subplots()
-    for i, position in enumerate(positionList):
-        meanOfMEAN[i], meanOfRMS[i] = plotSlice_Dai_0p8(ax_in_case, position, path2Data, dataDir, cut)
-
-    positionList = np.asarray(positionList)
-    ax_cases.plot(positionList/8.0,meanOfRMS, label=aliasDict[dataDir], marker=marker)
     
 def plotFor_caseList(caseList, path2Data, saveFigDir):
 #    positionList = [0,1,2,3,4,5,6,7,8,9,10,11,12,16,24,32,40,48,56,64,72,73,74,75]
