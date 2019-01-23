f60f8f553bcc087b094171df525f18cc2ac304c9
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-23 14:55:45 +0100

    re-arrange run.sh

--- pvpython/dimensioned_slice/run.sh
+++ pvpython/dimensioned_slice/run.sh
@@ -5,7 +5,7 @@ Newtonian=/store/8simu_tmp/shape_square/2a_3_T/Newtonian
 
 contourUx=Ux_mean_slice_yNormal.py
 contourUy=Uy_mean_slice_yNormal.py
-contourUy=Uz_mean_slice_yNormal.py
+contourUz=Uz_mean_slice_yNormal.py
 
 #pvpython $contourUy $BirdCarreau/inlet_0p5 -0.003 neg3mm &&
 #pvpython $contourUy $BirdCarreau/inlet0p5_impinging -0.003 neg3mm &&
@@ -15,14 +15,14 @@ contourUy=Uz_mean_slice_yNormal.py
 #pvpython $contourUy $Newtonian/Re2400 -0.003 neg3mm &&
 #pvpython $contourUy $Newtonian/Re4000 -0.003 neg3mm &&
 #pvpython $contourUy $Newtonian/Re4000_impinging -0.003 neg3mm &&
-pvpython $contourUy $BirdCarreau/inlet_0p5 0.003 pos3mm &&
-pvpython $contourUy $BirdCarreau/inlet0p5_impinging 0.003 pos3mm &&
-pvpython $contourUy $BirdCarreau/inlet_0p3 0.003 pos3mm &&
-pvpython $contourUy $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.003 pos3mm &&
-pvpython $contourUy $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.003 pos3mm &&
-pvpython $contourUy $Newtonian/Re2400 0.003 pos3mm &&
-pvpython $contourUy $Newtonian/Re4000 0.003 pos3mm &&
-pvpython $contourUy $Newtonian/Re4000_impinging 0.003 pos3mm &&
+#pvpython $contourUy $BirdCarreau/inlet_0p5 0.003 pos3mm &&
+#pvpython $contourUy $BirdCarreau/inlet0p5_impinging 0.003 pos3mm &&
+#pvpython $contourUy $BirdCarreau/inlet_0p3 0.003 pos3mm &&
+#pvpython $contourUy $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.003 pos3mm &&
+#pvpython $contourUy $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.003 pos3mm &&
+#pvpython $contourUy $Newtonian/Re2400 0.003 pos3mm &&
+#pvpython $contourUy $Newtonian/Re4000 0.003 pos3mm &&
+#pvpython $contourUy $Newtonian/Re4000_impinging 0.003 pos3mm &&
 pvpython $contourUy $BirdCarreau/inlet_0p5 0 0mm &&
 pvpython $contourUy $BirdCarreau/inlet0p5_impinging 0 0mm &&
 pvpython $contourUy $BirdCarreau/inlet_0p3 0 0mm &&
