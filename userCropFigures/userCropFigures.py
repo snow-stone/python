1185cca8915a36c4a7b0de6424c0cc41c63f786f
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-01-24 20:17:26 +0100

    update database

--- userCropFigures/userCropFigures.py
+++ userCropFigures/userCropFigures.py
@@ -1,6 +1,6 @@
 
 from PIL import Image
-import sys
+#import sys
  
 def crop(image_path, coords, saved_location):
     """
@@ -27,6 +27,14 @@ def main():
             'BirdCarreau/inlet_0p3-a_0p5-setT_St_1',
             'BirdCarreau/inlet_0p3-a_0p5-setT_St_5'
            ]
+
+    pathsNonNewtonian = [
+            'BirdCarreau/inlet_0p5',
+            'BirdCarreau/inlet0p5_impinging',
+            'BirdCarreau/inlet_0p3',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_1',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_5'
+           ]
 #    paths = [
 #            'BirdCarreau/inlet_0p5',
 #            'BirdCarreau/inlet_0p3'
@@ -38,7 +46,10 @@ def main():
 #            '2DstreamLines_4p0D',
 #            '2DstreamLines_6p0D'            
 #            ]
-    imageNames = ['Ux_mean_slice_yNomal_y_Eq_0mm_addInlet2']
+    imageNames = [#'nu_mean_slice_yNomal_y_Eq_neg3mm_addInlet2',
+                  #'nu_mean_slice_yNomal_y_Eq_0mm_addInlet2',
+                  'nu_mean_slice_x_Eq_6.0D',
+                  'nu_mean_slice_x_Eq_8.0D']
     
     coords_dict= {
             'k_mean' : (729, 660, 2496, 1173),
@@ -50,12 +61,31 @@ def main():
             '2DstreamLines_4p0D' : (522, 408, 1695, 1491),
             '2DstreamLines_6p0D' : (522, 408, 1695, 1491),
             'slice_6D_T_mean'    : (792, 264, 2097, 1566),
+            'nu_mean_slice_zNomal_z_Eq_0mm' : (600, 684, 2300, 1176),
+            'nu_mean_slice_x_Eq_0.0D'       : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_0.25D'      : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_0.5D'       : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_2.0D'       : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_4.0D'       : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_6.0D'       : (810, 285, 2082, 1551),
+            'nu_mean_slice_x_Eq_8.0D'       : (810, 285, 2082, 1551),
             'Ux_mean_slice_yNomal_y_Eq_neg3mm_addInlet2' : (552, 732, 2238, 1080),
-            'Ux_mean_slice_yNomal_y_Eq_0mm_addInlet2'    : (552, 732, 2238, 1080)
+            'Ux_mean_slice_yNomal_y_Eq_0mm_addInlet2'    : (552, 732, 2238, 1080),
+            'Ux_mean_slice_yNomal_y_Eq_pos3mm_addInlet2' : (552, 732, 2238, 1080),
+            'Uy_mean_slice_yNomal_y_Eq_neg3mm_addInlet2'    : (594, 750, 2298, 1077),
+            'Uy_mean_slice_yNomal_y_Eq_0mm_addInlet2'       : (594, 750, 2298, 1077),
+            'Uy_mean_slice_yNomal_y_Eq_pos3mm_addInlet2'    : (594, 750, 2298, 1077),
+            'Uz_mean_slice_yNomal_y_Eq_neg3mm_addInlet2'    : (594, 750, 2298, 1077),
+            'Uz_mean_slice_yNomal_y_Eq_0mm_addInlet2'       : (594, 750, 2298, 1077),
+            'Uz_mean_slice_yNomal_y_Eq_pos3mm_addInlet2'    : (594, 750, 2298, 1077),
+            'nu_mean_slice_yNomal_y_Eq_neg3mm_addInlet2'    : (621, 765, 2280, 1076),
+            'nu_mean_slice_yNomal_y_Eq_0mm_addInlet2'       : (621, 765, 2280, 1076),
+            'nu_mean_slice_yNomal_y_Eq_pos3mm_addInlet2'    : (621, 765, 2280, 1076)
             }    
     
     
-    for i, path in enumerate(paths):
+#    for i, path in enumerate(paths):
+    for i, path in enumerate(pathsNonNewtonian):
         for imageName in imageNames:
             image = dataBase + '/' + path + '/' + imageName + '.png'
             crop(image, coords_dict[imageName], dataBase + '/' + path + '/' + imageName + '_cropped.png')
--- userDrawOnFigure/userDrawOnFigure.py
+++ userDrawOnFigure/userDrawOnFigure.py
@@ -22,16 +22,36 @@ def main():
             'BirdCarreau/inlet_0p3-a_0p5-setT_St_1',
             'BirdCarreau/inlet_0p3-a_0p5-setT_St_5'
            ]
-
-    imageNames = [#'Ux_mean_slice_yNomal_y_Eq_neg3mm',
-                  'Ux_mean_slice_yNomal_y_Eq_0mm']
+           
+    pathsNonNewtonian = [
+            'BirdCarreau/inlet_0p5',
+            'BirdCarreau/inlet0p5_impinging',
+            'BirdCarreau/inlet_0p3',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_1',
+            'BirdCarreau/inlet_0p3-a_0p5-setT_St_5'
+           ]
+           
+    imageNames = ['nu_mean_slice_yNomal_y_Eq_neg3mm',
+                  'nu_mean_slice_yNomal_y_Eq_0mm',
+                  'nu_mean_slice_yNomal_y_Eq_pos3mm']
     
     xy_dict= {
             'Ux_mean_slice_yNomal_y_Eq_neg3mm' : [(888,755), (1219.5, 1078.5)],
-            'Ux_mean_slice_yNomal_y_Eq_0mm' : [(888,755), (1219.5, 1078.5)]
+            'Ux_mean_slice_yNomal_y_Eq_0mm' : [(888,755), (1219.5, 1078.5)],
+            'Ux_mean_slice_yNomal_y_Eq_pos3mm' : [(888,755), (1219.5, 1078.5)],
+            'Uy_mean_slice_yNomal_y_Eq_neg3mm' : [(909,760.5), (1215, 1072.5)],
+            'Uy_mean_slice_yNomal_y_Eq_0mm' : [(909,760.5), (1215, 1072.5)],
+            'Uy_mean_slice_yNomal_y_Eq_pos3mm' : [(909,760.5), (1215, 1072.5)],
+            'Uz_mean_slice_yNomal_y_Eq_neg3mm' : [(909,760.5), (1215, 1072.5)],
+            'Uz_mean_slice_yNomal_y_Eq_0mm' : [(909,760.5), (1215, 1072.5)],
+            'Uz_mean_slice_yNomal_y_Eq_pos3mm' : [(909,760.5), (1215, 1072.5)],
+            'nu_mean_slice_yNomal_y_Eq_neg3mm' : [(928.5, 760), (1234.5, 1075)],
+            'nu_mean_slice_yNomal_y_Eq_0mm' : [(928.5, 760), (1234.5, 1075)],
+            'nu_mean_slice_yNomal_y_Eq_pos3mm' : [(928.5, 760), (1234.5, 1075)]                                                  
             }
             
-    for i, path in enumerate(paths):
+#    for i, path in enumerate(paths):
+    for i, path in enumerate(pathsNonNewtonian):
         for imageName in imageNames:
             imageFullPath = dataBase + '/' + path + '/' + imageName + '.png'
             addInlet2(imageFullPath, xy_dict[imageName], saveLocation = dataBase + '/' + path, saveAs= imageName +'_addInlet2'+'.png')
