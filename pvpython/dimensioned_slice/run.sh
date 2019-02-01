3c52bd9b98f0be3f5cb7aa9eb45ed6cc965bb888
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-01 09:21:12 +0100

    for contourNu_z

--- pvpython/dimensioned_slice/run.sh
+++ pvpython/dimensioned_slice/run.sh
@@ -317,18 +317,21 @@ contourT_z=T_mean_slice_zNormal_noClipped.py
 #echo "Finish T_x 8.0D" &&
 #echo "---------------" 
 
-#pvpython $contourNu_z $BirdCarreau/inlet_0p5 &&
-#pvpython $contourNu_z $BirdCarreau/inlet0p5_impinging &&
-#pvpython $contourNu_z $BirdCarreau/inlet_0p3 &&
-#pvpython $contourNu_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 &&
-#pvpython $contourNu_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 
+pvpython $contourNu_z $BirdCarreau/inlet_0p5 &&
+pvpython $contourNu_z $BirdCarreau/inlet0p5_impinging &&
+pvpython $contourNu_z $BirdCarreau/inlet_0p3 &&
+pvpython $contourNu_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 &&
+pvpython $contourNu_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 
 
 
-pvpython $contourT_z $BirdCarreau/inlet_0p5 0 0mm &&
-pvpython $contourT_z $BirdCarreau/inlet0p5_impinging 0 0mm &&
-pvpython $contourT_z $BirdCarreau/inlet_0p3 0 0mm &&
-pvpython $contourT_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0 0mm &&
-pvpython $contourT_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0 0mm &&
-pvpython $contourT_z $Newtonian/Re2400 0 0mm &&
-pvpython $contourT_z $Newtonian/Re4000 0 0mm &&
-pvpython $contourT_z $Newtonian/Re4000_impinging 0 0mm 
+#pvpython $contourT_z $BirdCarreau/inlet_0p5 0 0mm &&
+#pvpython $contourT_z $BirdCarreau/inlet0p5_impinging 0 0mm &&
+#pvpython $contourT_z $BirdCarreau/inlet_0p3 0 0mm &&
+#pvpython $contourT_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0 0mm &&
+#pvpython $contourT_z $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0 0mm &&
+#pvpython $contourT_z $Newtonian/Re2400 0 0mm &&
+#pvpython $contourT_z $Newtonian/Re4000 0 0mm &&
+#pvpython $contourT_z $Newtonian/Re4000_impinging 0 0mm 
+
+#pvpython $contourNu_z $BirdCarreau/inlet_0p5 
+#pvpython $contourT_z $BirdCarreau/inlet_0p5 0 0mm 
