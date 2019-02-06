22a2dfef6e0f2f6483fe7cff060e64f5c5d8d573
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-06 11:13:20 +0100

    removing other plots

--- BirdCarreau_PowerLaw.py
+++ BirdCarreau_PowerLaw.py
@@ -22,7 +22,7 @@ a=2 # defaut BirdCarreau in OpenFOAM
 nu = nuInf + (nu0-nuInf)*(1.0+(k*gama)**a)**((n-1.0)/a)
 
 n1=0.6
-nu1= nu0 * gama**(n1-1.0)
+#nu1= nu0 * gama**(n1-1.0)
 
 
 
@@ -30,35 +30,35 @@ fig,ax = plt.subplots()
 
 
 ax.plot(gama,nu,linewidth=2,label='nu')
-ax.plot(gama,nu1,linewidth=2,label='nu1')
+#ax.plot(gama,nu1,linewidth=2,label='nu1')
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.legend()
 
 ########################################################################
-fig1,ax1 = plt.subplots()
-
-gama = np.linspace(0.1,100000,10000000)
-
-nu0=5e-6
-nuInf=2e-7
-
-k=1
-n=0.326
-
-a=2 # defaut BirdCarreau in OpenFOAM
-
-#nu = gama**2
-nu = nuInf + (nu0-nuInf)*(1.0+(k*gama)**a)**((n-1.0)/a)
-
-n=0.6
-nu1 = nuInf + (nu0-nuInf)*(1.0+(k*gama)**a)**((n-1.0)/a)
-
-#n1=0.6
-#nu1= nu0 * gama**(n1-1.0)
-ax1.plot(gama,nu,linewidth=2,label='nu')
-ax1.plot(gama,nu1,linewidth=2,label='nu1')
-ax1.set_xscale("log")
-ax1.set_yscale("log")
-ax1.legend()
+#fig1,ax1 = plt.subplots()
+#
+#gama = np.linspace(0.1,100000,10000000)
+#
+#nu0=5e-6
+#nuInf=2e-7
+#
+#k=1
+#n=0.326
+#
+#a=2 # defaut BirdCarreau in OpenFOAM
+#
+##nu = gama**2
+#nu = nuInf + (nu0-nuInf)*(1.0+(k*gama)**a)**((n-1.0)/a)
+#
+#n=0.6
+#nu1 = nuInf + (nu0-nuInf)*(1.0+(k*gama)**a)**((n-1.0)/a)
+#
+##n1=0.6
+##nu1= nu0 * gama**(n1-1.0)
+#ax1.plot(gama,nu,linewidth=2,label='nu')
+#ax1.plot(gama,nu1,linewidth=2,label='nu1')
+#ax1.set_xscale("log")
+#ax1.set_yscale("log")
+#ax1.legend()
 
