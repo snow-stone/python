965db249507be89c46fa31cd9cb7b7de49f2d942
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-13 11:14:11 +0100

    add fft

--- userProbeByLabel_history/userProbeByLabel_history_fft.py
+++ userProbeByLabel_history/userProbeByLabel_history_fft.py
@@ -8,7 +8,7 @@ Created on Wed Dec 12 21:08:57 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-def userProbeByLabel_forcing(ax, caseName, path2Data, fieldName, sample, positions, color, cut=0.):
+def getProbeByLabel_forcing(caseName, path2Data, fieldName, sample, positions, color, cut=0.):
     RelativeDataFile = "./"+"userDefinedLog/history_labelGroup_"+fieldName+".old"
     rawData = np.genfromtxt(path2Data+"/"+caseName+'/'+RelativeDataFile)
 #    print rawData.shape
@@ -19,7 +19,7 @@ def userProbeByLabel_forcing(ax, caseName, path2Data, fieldName, sample, positio
     std = np.std(probeData[:,sample])
     mean = np.mean(probeData[:,sample])
 #    print position_in_D + " std : ", std, " mean : ", mean
-    ax.plot(time, probeData[:,sample], color=color, linewidth=0.5, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[:])), linestyle='-')   
+#    ax.plot(time, probeData[:,sample], color=color, linewidth=0.5, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[:])), linestyle='-')   
 
     RelativeDataFile = "./"+"userDefinedLog/history_labelGroup_"+fieldName
     rawData = np.genfromtxt(path2Data+"/"+caseName+'/'+RelativeDataFile)
@@ -32,12 +32,13 @@ def userProbeByLabel_forcing(ax, caseName, path2Data, fieldName, sample, positio
     std = np.std(probeData[cutSliceIndex:,sample])
     mean = np.mean(probeData[cutSliceIndex:,sample])
 #    print position_in_D + " std : ", std, " mean : ", mean
-    ax.plot(time[cutSliceIndex:], probeData[cutSliceIndex:,sample], color=color, linewidth=2, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[cutSliceIndex:])), linestyle='-')
-    ax.plot(time[:cutSliceIndex], probeData[:cutSliceIndex,sample], color=color, linewidth=1)    
+#    ax.plot(time[cutSliceIndex:], probeData[cutSliceIndex:,sample], color=color, linewidth=2, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[cutSliceIndex:])), linestyle='-')
+#    ax.plot(time[:cutSliceIndex], probeData[:cutSliceIndex,sample], color=color, linewidth=1)    
 
-    return std, mean
+#    return std, mean
+    return probeData[cutSliceIndex:,sample]
 
-def userProbeByLabel(ax, caseName, path2Data, fieldName, sample, positions, color, cut=0.5):
+def getProbeByLabel(caseName, path2Data, fieldName, sample, positions, color, cut=0.5):
     RelativeDataFile = "./"+"userDefinedLog/history_labelGroup_"+fieldName
     rawData = np.genfromtxt(path2Data+"/"+caseName+'/'+RelativeDataFile)
 #    print rawData.shape
@@ -49,10 +50,11 @@ def userProbeByLabel(ax, caseName, path2Data, fieldName, sample, positions, colo
     std = np.std(probeData[cutSliceIndex:,sample])
     mean = np.mean(probeData[cutSliceIndex:,sample])
 #    print position_in_D + " std : ", std, " mean : ", mean
-    ax.plot(time[cutSliceIndex:], probeData[cutSliceIndex:,sample], color=color, linewidth=2, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[cutSliceIndex:])), linestyle='-')
-    ax.plot(time[:cutSliceIndex], probeData[:cutSliceIndex,sample], color=color, linewidth=1)    
+#    ax.plot(time[cutSliceIndex:], probeData[cutSliceIndex:,sample], color=color, linewidth=2, label=caseName+"_"+position_in_D+"\nNbSampleEq_"+str(len(time[cutSliceIndex:])), linestyle='-')
+#    ax.plot(time[:cutSliceIndex], probeData[:cutSliceIndex,sample], color=color, linewidth=1)    
     
-    return std, mean
+#    return std, mean\
+    return probeData[cutSliceIndex:,sample]
 
 ##==============================================================================
 ##   RMS 
@@ -109,7 +111,7 @@ def main():
              "BirdCarreau"+"/"+"inlet_0p3",
              "Newtonian"+"/"+"Re2400",
              "BirdCarreau/inlet_0p3-a_0p5-setT_St_1",  # j=6, involving userProbeByLabel_forcing
-		  "BirdCarreau/inlet_0p3-a_0p5-setT_St_5",   # j=7, involving userProbeByLabel_forcing
+		    "BirdCarreau/inlet_0p3-a_0p5-setT_St_5",   # j=7, involving userProbeByLabel_forcing
              "BirdCarreau"+"/"+"inlet_0p5",
              "Newtonian"+"/"+"Re4000",
              "BirdCarreau/inlet0p5_impinging",
@@ -124,7 +126,8 @@ def main():
 #             "Newtonian"+"/"+"Re2400"
 #            ]
               
-    fieldNames=["U_x", "U_y", "U_z", "T"]
+#    fieldNames=["U_x", "U_y", "U_z", "T"]
+    fieldNames=["T"]
 
     aliasDict={
         "BirdCarreau/inlet_0p3":r'$NN^{1}_{d}$',
@@ -147,66 +150,80 @@ def main():
     for k, position in enumerate(positionSubSet):
         p=positionSubSet[k]
         print "position : " , allProbePosition[p]
-        #figsize should be in inches.
-        fig, axses_case = plt.subplots(len(cases), 1, sharex=True, figsize=(20,10))# figsize is an additional parameter for class matplotlib.pyplot.subplots: passed by **fig_kw
-        mean = np.zeros((len(allProbePosition),len(cases)))
-        std = np.zeros((len(allProbePosition),len(cases)))
+#        fig, axses_case = plt.subplots(len(cases), 1, sharex=True, figsize=(20,10))# figsize is an additional parameter for class matplotlib.pyplot.subplots: passed by **fig_kw
+#        mean = np.zeros((len(allProbePosition),len(cases)))
+#        std = np.zeros((len(allProbePosition),len(cases)))
         for j, case in enumerate(cases):
             for i, fieldName in enumerate(fieldNames):
-        #        mean = np.zeros((len(positionList),4))
-        #        std = np.zeros((len(positionList),4))
-        #        mean, std = plotField(axarr, fieldName, cases, positionList, colorList, samples, field=i)
-        #        plotField(axarr, fieldName, cases, positionList, colorList, samples, field=i)
-        #        spatial_mean_rms(arrayName, cases, positionList, mean, std, linestyleList, markerList)
-                axses_case[j].set_ylim(-0.5,2)
-                axses_case[j].set_yticks([0, 1])
-                axses_case[j].tick_params(axis='y', direction='in', length=4, width=1.5)
-                axses_case[j].set_xlim(0,0.85)
+#                axses_case[j].set_ylim(-0.5,2)
+#                axses_case[j].set_yticks([0, 1])
+#                axses_case[j].tick_params(axis='y', direction='in', length=4, width=1.5)
+#                axses_case[j].set_xlim(0,0.85)
                 
                 print "axe number = ", j , fieldName
+#                if j == 0:
+#                    if allProbePosition[p] >= 24:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.6)
+#                    else:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.6)
+#
+#                elif j == 1:
+#                    if allProbePosition[p] >= 24:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                    else:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                elif j == 2 :
+#                    std[i,j], mean[i,j] = userProbeByLabel_forcing(axses_case[j], case, path2Data,  fieldName, p, allProbePosition, colorList[i], cut=0.35)
+#                elif j == 3 :
+#                    std[i,j], mean[i,j] = userProbeByLabel_forcing(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.46)
+#
+#                elif j == 4:
+#                    if allProbePosition[p] >= 7:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                    else :
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                elif j == 5 :
+#                    if allProbePosition[p] >= 24:
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                    else :
+#                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                elif j == 6 :
+#                    std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
+#                elif j == 7 :
+#                    std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)            
+#                else :
+#                    print "There's a big problem"
+                
                 if j == 0:
-                    if allProbePosition[p] >= 24:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.6)
-                    else:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.6)
+                    data = getProbeByLabel(case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.6)
 
-                elif j == 1:
-                    if allProbePosition[p] >= 24:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                    else:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                elif j == 2 :
-                    std[i,j], mean[i,j] = userProbeByLabel_forcing(axses_case[j], case, path2Data,  fieldName, p, allProbePosition, colorList[i], cut=0.35)
-                elif j == 3 :
-                    std[i,j], mean[i,j] = userProbeByLabel_forcing(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.46)
+#                if j == 7 :
+#                    data = getProbeByLabel(case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
 
-                elif j == 4:
-                    if allProbePosition[p] >= 7:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                    else :
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                elif j == 5 :
-                    if allProbePosition[p] >= 24:
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                    else :
-                        std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                elif j == 6 :
-                    std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)
-                elif j == 7 :
-                    std[i,j], mean[i,j] = userProbeByLabel(axses_case[j], case, path2Data, fieldName, p, allProbePosition, colorList[i], cut=0.5)            
-                else :
-                    print "There's a big problem"
-        
 #        spatial_mean_rms(fieldName, cases, allProbePosition, mean, std, linestyleList, markerList)
 #        fig.text(0.5, 0.04, r'$t$', ha='center', va='center', fontsize=20)
-        axses_case[-1].set_xlabel(r'$t$')
-        x = 0.82
-        yStart = 0.82
-        for i, case in enumerate(cases):
-            y = yStart - i * 0.0975
-            fig.text(x, y, aliasDict[case], fontsize=30)
+#        axses_case[-1].set_xlabel(r'$t$')
+#        x = 0.82
+#        yStart = 0.82
+#        for i, case in enumerate(cases):
+#            y = yStart - i * 0.0975
+#            fig.text(x, y, aliasDict[case], fontsize=30)
+#
+##        fig.suptitle(r'Time history of $Ux,\, Uy,\, Uz,\, T$ @ position '+str(allProbePosition[p]/8.0)+'D')
+#        fig.savefig(path2Data+"/"+"PICTURE_history_FFT/"+"8simu/"+str(allProbePosition[p]/8.0)+"D.png", bbox_inches='tight') # bbox_inches = 'tight' is neccessary
 
-#        fig.suptitle(r'Time history of $Ux,\, Uy,\, Uz,\, T$ @ position '+str(allProbePosition[p]/8.0)+'D')
-        fig.savefig(path2Data+"/"+"PICTURE_history/"+"8simu/"+str(allProbePosition[p]/8.0)+"D.png", bbox_inches='tight') # bbox_inches = 'tight' is neccessary
+#    data = np.append(data, data[0])
+    A = np.fft.rfft(data)
+    print len(data)
+    print len(A)
+#        plt.subplots(2)
+    fig, ax = plt.subplots()
+    ax.plot(data)
+    fig, ax = plt.subplots()
+    ax.plot(A.real[:100])
+    fig, ax = plt.subplots()
+    ax.plot(A.imag[:100])
+#    print np.fft.rfft(data).imag
+        
 
 main()
