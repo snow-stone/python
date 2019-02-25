6c6e01fda217357b8692e1e0286c2154f49631b3
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-25 21:43:14 +0100

    add data from 6.5 to 7

---
+++ Analysis/Tools/spatialSeries_returnBothInnerAndOuter/parameters_t_c.py
@@ -0,0 +1,37 @@
+
+# physical parameters
+
+physics={
+        'R':0.004,
+        'nu':1.0e-6,
+        'uTau':0.051
+        }
+
+#print physics
+
+sampling={
+        'raw_sample_size':160,
+        #'dataShape':(200,4)     # uniform
+        'dataShape':(99,4)       # face
+        }
+
+# data entry parameters
+
+dataEntry={
+        'timeStep':6.5,
+        'startTime':6.5,  # KinecticEnergy stationary
+        'endTime':7,
+        'chunkStep':10,
+        'NbOfFiles':51,
+        'path':"pipe4",
+        }
+
+parameters={
+        'physics':physics,
+        'sampling':sampling,
+        'dataEntry':dataEntry,
+        }
+
+
+# secondary physical parameters
+#Retau=uTau*R/nu
