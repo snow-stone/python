6696d459cd082e7e027e62268ed8fca711485104
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-25 17:41:47 +0100

    improve plot

--- Analysis/Tools/spatialSeries/spatialSeries0_fig1_M1_typeUniform_typeFace_module.py
+++ Analysis/Tools/spatialSeries/spatialSeries0_fig1_M1_typeUniform_typeFace_module.py
@@ -2,22 +2,22 @@ import numpy as np
 import general_settings as gs
 import reference_database as rdb
 import matplotlib
-matplotlib.use("agg")
+matplotlib.use("agg") 
 import matplotlib.pyplot as plt
 
 def addToFig1(ax):        
     DNS_x, DNS_y = rdb.Eggels1994_thesis_Fig4p7_DNS()
     
-    ax.plot(DNS_x,DNS_y,linewidth=gs.lw,label='DNS_Eggels')
+    ax.plot(DNS_x,DNS_y,linewidth=2,label=r'$DNS_{E}$')
     
 #    a_x1, a_y = rdb.analytic_Uz_meanProfile(True, 0.1, 100)
 #    ax.plot(a_x,a_y,linewidth=1.5,label='Analytic')
     
     x1 = np.arange(1,15,1)
-    ax.plot(x1, x1, linewidth=gs.lw, linestyle='--', color='black')#,label='viscous layer')
+    ax.plot(x1, x1, linewidth=4, linestyle='--', color='black')#,label='viscous layer')
     
     x2 = np.arange(5,200,1)
-    ax.plot(x2, 2.5*np.log(x2)+5.5, linewidth=gs.lw, linestyle='--',color='black')#, label='log layer')
+    ax.plot(x2, 2.5*np.log(x2)+5.5, linewidth=4, linestyle='--',color='black')#, label='log layer')
     
 #    print "DNS_x[0],DNS_y[0]",DNS_x[0],DNS_y[0]
 #    print "DNS_x[1],DNS_y[1]",DNS_x[1],DNS_y[1]
@@ -44,7 +44,7 @@ def main():
     import parameters_periodic_M1_dict as M1
     import copy
 
-    fig1,ax1 = plt.subplots()
+    fig1,ax1 = plt.subplots(figsize=(20,10))
     l = ssR.pre_check(M1.parameters,'lines_typeUniform_cell')
     innerCoordData = ssR.process(M1.parameters,validDataList=l,colonNb=3,innerVar=True)
     ax1.plot(innerCoordData['rPlus'],innerCoordData['mean'],label='run0 t=%.1f M1 unif'%M1.parameters['dataEntry']['timeStep'],linewidth=gs.lw)
@@ -65,12 +65,15 @@ def main():
     innerCoordData = ssR.process(dict_F,validDataList=l,colonNb=3,innerVar=True) 
     ax1.plot(innerCoordData['rPlus'],innerCoordData['mean'],label='run0 t=%.1f M1 face'%dict_F['dataEntry']['timeStep'],linewidth=gs.lw)
 
+    ax1.tick_params(axis='both', which='major', direction='out', length=8, width=4)
+    ax1.tick_params(axis='x', which='minor', direction='out', length=8, width=2)
+
     addToFig1(ax1)
     ax1.legend(bbox_to_anchor=(0.55, 1.0), ncol=1, fancybox=True, shadow=True)
     ax1.set_xscale('log')
     ax1.set_xlim(1,200)
-    ax1.set_xlabel(r'$r^+$',fontsize=gs.sizeLabel)
-    ax1.set_ylabel(r'$U_z^+$',fontsize=gs.sizeLabel)
+    ax1.set_xlabel(r'$r^+$')
+    ax1.set_ylabel(r'$U_z^+$')
     ax1.set_title('spatial stat.')
 
     #plt.show()
