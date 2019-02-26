3e88c733bb3580a78b978e3889aba7d7332a027e
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-26 09:13:00 +0100

    improve plot

--- Analysis/Tools/spatialSeries_returnBothInnerAndOuter/UzMean.py
+++ Analysis/Tools/spatialSeries_returnBothInnerAndOuter/UzMean.py
@@ -18,25 +18,21 @@ def main():
     rc('text', usetex=True)
 
     import spatialSeriesReader_returnBothInnerAndOuter as ssR
-    #import parameters_pipe_Nperiodic_15D_5Dforced as pNp_15D5Df_M0
     import parameters_t_c as pNp_15D5Df_M0
     import copy
 
     sampleNaming='lines_typeFace_cell'
-#    sampleNaming='lines_typeUniform_cell'
-#    sampleNaming='lines_complement0_typeFace_cell'
     
     fig3,ax3 = plt.subplots(figsize=(20,10))
     y_, Uz = rdb.Eggels_thesis.Fig4p5_DNS()
-    ax3.plot(y_,Uz,label=r'$DNS_E$',linewidth=2)
+    ax3.plot(y_,Uz,label=r'$DNS_E$',linewidth=1,linestyle='--',marker='s',color='mediumvioletred',markeredgecolor='mediumvioletred', markerfacecolor='none', markersize=16, markeredgewidth=4)
     y1_, Uz1 = rdb.Eggels_thesis.Fig4p5_HWA()
-    ax3.plot(y_[::2],Uz[::2],label=r'$HWA$',linewidth=2,marker='s',markerfacecolor='none')
+    ax3.plot(y_[::2],Uz[::2],label=r'$HWA$',linewidth=1,linestyle='--',marker='s',color='forestgreen',markeredgecolor='forestgreen', markerfacecolor='none', markersize=16, markeredgewidth=4)
     
     
     l = ssR.pre_check(pNp_15D5Df_M0.parameters,sampleNaming)
     outerCoordData = ssR.process(pNp_15D5Df_M0.parameters,validDataList=l,colonNb=3,innerVar=False)  
-    #ax3.plot(outerCoordData['rByD'][::2],outerCoordData['mean'][::2],label='run3 t=%.1f M0'%pNp_15D5Df_M0.parameters['dataEntry']['timeStep'],linewidth=gs.lw)
-    ax3.plot(outerCoordData['rByD'][::2],outerCoordData['mean'][::2],label='simu',linewidth=gs.lw)
+    ax3.plot(outerCoordData['rByD'][::2],outerCoordData['mean'][::2],label='simu',linewidth=4,color='darkcyan')
     
     ax3.set_xlabel(r'$(R-r)/D$')
     ax3.set_ylabel(r'$\overline{u_z}/\overline{u_z}_{max}$')
