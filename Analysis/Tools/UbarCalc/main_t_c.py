c49801b23b0251b0ead99e7d7662b2e14288f27d
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-27 09:32:41 +0100

    update for t_c

--- Analysis/Tools/UbarCalc/main_t_c.py
+++ Analysis/Tools/UbarCalc/main_t_c.py
@@ -5,31 +5,36 @@ def main():
     import matplotlib
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
-    
-#    import parameters_periodic_M1_bNV as pp_M1_bNV
-#    import parameters_periodic_M0_bNV as pp_M0_bNV
-    import parameters_t_c as pp_M0_bNV
 
-    fig,ax = plt.subplots()
+    plt.style.use('seaborn-white') # from defaut
+    plt.rcParams.update({'font.size': 30})
+    plt.rcParams['savefig.dpi'] = 100
+    
+    from matplotlib import rc
+    #    rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+    ## for Palatino and other serif fonts use:
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    
+    import parameters_t_c as t_c
+
+    fig,ax = plt.subplots(figsize=(16,10))
     
     logFileNameList=[]
     N=5
     for i in range(N):
-        logFileNameList.append('log.patchIntegrate_Inlet'+str(i))
+        logFileNameList.append('log.patchIntegrateSerial_Inlet'+str(i))
 
-    area1, time1, flux1 = UbarCalc.patchesIntegrate(pp_M0_bNV.parameters,logFileNameList)
+    area1, time1, flux1 = UbarCalc.patchesIntegrate(t_c.parameters,logFileNameList)
     print "area1 = ", area1
     time1=time1[1:]
-#    print "len(time1) : ", len(time1)
-#    print "len(flux1) : ", len(flux1)
-    ax.plot(time1,np.abs(flux1)/area1,label='run2 M0 patchIntegrate')
+    ax.plot(time1,np.abs(flux1)/area1,label=r'$<u_x>_{inlet}$')
     
-#    ax.set_xlim(104,120)
+    ax.set_xlim(0,7)
 #    ax.set_ylim(0.7,1.3)
-    ax.set_xlabel(r'$t$',fontsize=gs.sizeLabel)
-    ax.set_ylabel(r'$<Uz>_{volume}$',fontsize=gs.sizeLabel)
-    ax.legend(bbox_to_anchor=(1.6, 1), ncol=1, fancybox=True, shadow=True)
-#    ax.set_title('Volumic average on Uz through whole volume')
+    ax.set_xlabel(r'$t$')
+    ax.set_ylabel(r'$<u_x>_{inlet}$')
+    ax.legend(bbox_to_anchor=(1, 1), ncol=1, fancybox=True, shadow=True)
 
     fig.savefig('Ubar_t_c.png', bbox_inches='tight')
 
