b2584c7057e119ae1a3282b31c97a8fbc5e2f6d0
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-03-18 21:34:15 +0100

    update

--- pvpython/export_csv/run.sh
+++ pvpython/export_csv/run.sh
@@ -3,7 +3,7 @@
 BirdCarreau=/store/8simu_tmp/shape_square/2a_3_T/BirdCarreau
 Newtonian=/store/8simu_tmp/shape_square/2a_3_T/Newtonian
 
-export=export_csv_slice_xNormal.py
+#export=export_csv_slice_xNormal.py
 export_nu=nu_mean_export_csv_slice_xNormal.py
 
 #pvpython $export_nu $BirdCarreau/inlet_0p5 0.0041 0.0D &&
@@ -30,6 +30,14 @@ export_nu=nu_mean_export_csv_slice_xNormal.py
 ##echo "---------------" &&
 ##echo "Finish 0.5D" &&
 ##echo "---------------"
+pvpython $export_nu $BirdCarreau/inlet_0p5                 0.012 1.0D &&
+pvpython $export_nu $BirdCarreau/inlet0p5_impinging        0.012 1.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3                 0.012 1.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.012 1.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.012 1.0D &&
+echo "---------------" &&
+echo "Finish 1.0D" &&
+echo "---------------" &&
 #pvpython $export_nu $BirdCarreau/inlet_0p5 0.02 2.0D &&
 #pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.02 2.0D &&
 #pvpython $export_nu $BirdCarreau/inlet_0p3 0.02 2.0D &&
@@ -38,6 +46,14 @@ export_nu=nu_mean_export_csv_slice_xNormal.py
 #echo "---------------" &&
 #echo "Finish 2.0D" &&
 #echo "---------------" &&
+pvpython $export_nu $BirdCarreau/inlet_0p5                 0.028 3.0D &&
+pvpython $export_nu $BirdCarreau/inlet0p5_impinging        0.028 3.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3                 0.028 3.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.028 3.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.028 3.0D &&
+echo "---------------" &&
+echo "Finish 3.0D" &&
+echo "---------------" &&
 #pvpython $export_nu $BirdCarreau/inlet_0p5 0.036 4.0D &&
 #pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.036 4.0D &&
 #pvpython $export_nu $BirdCarreau/inlet_0p3 0.036 4.0D &&
@@ -46,6 +62,14 @@ export_nu=nu_mean_export_csv_slice_xNormal.py
 #echo "---------------" &&
 #echo "Finish 4.0D" &&
 #echo "---------------"
+pvpython $export_nu $BirdCarreau/inlet_0p5                 0.044 5.0D &&
+pvpython $export_nu $BirdCarreau/inlet0p5_impinging        0.044 5.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3                 0.044 5.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.044 5.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.044 5.0D &&
+echo "---------------" &&
+echo "Finish 5.0D" &&
+echo "---------------" &&
 #pvpython $export_nu $BirdCarreau/inlet_0p5 0.052 6.0D &&
 #pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.052 6.0D &&
 #pvpython $export_nu $BirdCarreau/inlet_0p3 0.052 6.0D &&
@@ -54,6 +78,14 @@ export_nu=nu_mean_export_csv_slice_xNormal.py
 #echo "---------------" &&
 #echo "Finish 6D" &&
 #echo "---------------" &&
+pvpython $export_nu $BirdCarreau/inlet_0p5                 0.06 7.0D &&
+pvpython $export_nu $BirdCarreau/inlet0p5_impinging        0.06 7.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3                 0.06 7.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.06 7.0D &&
+pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.06 7.0D &&
+echo "---------------" &&
+echo "Finish 7.0D" &&
+echo "---------------"
 #pvpython $export_nu $BirdCarreau/inlet_0p5 0.068 8.0D &&
 #pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.068 8.0D &&
 #pvpython $export_nu $BirdCarreau/inlet_0p3 0.068 8.0D &&
@@ -70,11 +102,11 @@ export_nu=nu_mean_export_csv_slice_xNormal.py
 ##echo "---------------" &&
 ##echo "Finish 9D" &&
 ##echo "---------------"
-pvpython $export_nu $BirdCarreau/inlet_0p5 0.079 9.5D &&
-pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.079 9.5D &&
-pvpython $export_nu $BirdCarreau/inlet_0p3 0.079 9.5D &&
-pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.079 9.5D &&
-pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.079 9.5D &&
-echo "---------------" &&
-echo "Finish 9.5D" &&
-echo "---------------"
+#pvpython $export_nu $BirdCarreau/inlet_0p5 0.079 9.5D &&
+#pvpython $export_nu $BirdCarreau/inlet0p5_impinging 0.079 9.5D &&
+#pvpython $export_nu $BirdCarreau/inlet_0p3 0.079 9.5D &&
+#pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_1 0.079 9.5D &&
+#pvpython $export_nu $BirdCarreau/inlet_0p3-a_0p5-setT_St_5 0.079 9.5D &&
+#echo "---------------" &&
+#echo "Finish 9.5D" &&
+#echo "---------------"
