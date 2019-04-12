5a1ed128903abdb45aec7b24d7cee18cfbfc5080
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-12 15:26:08 +0200

    print info

--- python_rsync/read.py
+++ python_rsync/read.py
@@ -1,5 +1,26 @@
 import scipy.io as io
 
+def printBijection(database, aliasName):
+    
+    print "============================"
+    print "case alias : ", aliasName
+    print "source : "
+    print database[aliasName]['sourceDir']
+    print "target : "
+    print database[aliasName]['targetDir']
+    print ""
+
 d = io.loadmat('dataBase')
 
-print d
+print "read from *.mat"
+print "keys : ", d.keys()
+
+d.pop("__header__")
+d.pop("__globals__")
+d.pop("__version__")
+
+print "after removing extra keys other than case alias"
+print "keys : ", d.keys()
+
+for alias in d:
+    printBijection(d, alias)
