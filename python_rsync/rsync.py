510200198c6fa8b74374f26337238855cf2dd506
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-12 10:34:13 +0200

    alpha test passed : rsync succeed

--- python_rsync/rsync.py
+++ python_rsync/rsync.py
@@ -1,50 +1,30 @@
 import sys
 import commands, os
 
-def submitJob(cmd, slurmFile):
-    if os.path.exists(slurmFile):
-        print "submitting job using : %s" % cmd
-        status, jobnum = commands.getstatusoutput(cmd)
-        return status, jobnum.split()[-1]
-    else:
-        print "FATAL ERROR!"
-        print "file %s doesnt exits" % slurmFile
-        return (1, '0000')
+def submitJob(cmd):
+    print "submitting job using : %s" % cmd
+    status, jobOutput = commands.getstatusoutput(cmd)
+    print ""
+    print "===================="
+    if status == 0 :
+        print "job return value : 0"
+        print "job output :"
+        print jobOutput
+    else :
+        print "job failed"
+        print "job output :"
+        print jobOutput
 
-def tryJob(job_number, queue):
+    return status, jobOutput
 
-    simuLogFile = "logSimulation_"+str(job_number)
-    
-    slurmFile   = queue
-    if queue == 'test' :
-        with open(slurmFile,'w') as sf:
-            sf.write("#!/bin/bash\n")
-            sf.write("##SBATCH --job-name=someName\n")
-            sf.write("#SBATCH --output=job.%j.out\n")
-            sf.write("#SBATCH --error=job.%j.err\n")
-            sf.write("#SBATCH --mail-user=haining.luo@doctorant.ec-lyon.fr\n\n")
-            
-            sf.write("#SBATCH --partition=mononode\n")
-            sf.write("#SBATCH --mem-per-cpu=4000\n")
-            sf.write("#SBATCH --nodes=1\n")
-            sf.write("#SBATCH --ntasks-per-node=1\n")
-            sf.write("##SBATCH --exclusive\n")
-            sf.write("#SBATCH --cpus-per-task=1")
-            sf.write("#SBATCH --time=00:10:00\n\n")      # min is the smallest unit here
+def tryJob(job_number):
 
-            sf.write("module purge\n")
-            sf.write("module load OpenMPI/1.6.5-GCC-4.8.3 \n")
-            sf.write(". /home/lmfa/hluo/LocalSoftware/OpenFOAM/OpenFOAM-2.3.x/etc/bashrc.Opt\n\n")
-            
-            sf.write("sliceStore=/store/lmfa/fct/hluo/occigen/caseByGeometry/T/shape_square/2a_3_T/sliceStore\n")
-            sf.write("userLabelListOp_Dai T $sliceStore slice0 -time '0:0.6' > %s\n" % simuLogFile)
-        sf.close()
-
-    cmd = "sbatch %s" % slurmFile
-    status, jobID = submitJob(cmd, slurmFile)
+    sourceDir="/store/lmfa/fct/hluo/zaurak/caseByGeometry/T/new-mesh/pointwise/postProcessing/1b_mirrorMerge/postProcessing"
+    cmd = "rsync -av newton:%s 1b_mirrorMerge/" % sourceDir
+    status, jobOutput = submitJob(cmd)
 
 def main():
-    queue = sys.argv[1]
-    tryJob(1, queue)
+    #queue = sys.argv[1]
+    tryJob(1)
     
 main()
