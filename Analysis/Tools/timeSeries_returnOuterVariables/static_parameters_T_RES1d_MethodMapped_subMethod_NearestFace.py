05fba5e8591dfae27a80b1f5c24300bd063130bb
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-12 16:10:46 +0200

    test passed : cut1_new_static

---
+++ Analysis/Tools/timeSeries_returnOuterVariables/static_parameters_T_RES1d_MethodMapped_subMethod_NearestFace.py
@@ -0,0 +1,51 @@
+# static
+import json
+
+database=json.load(open("/home/hluo/work/git/python/python_rsync/database.txt"))
+
+#print database['D1-1d']['targetDir']
+
+# physical parameters
+
+physics={
+        'R':0.004,
+        'nu':1.0e-6,
+        'uTau':0.0473
+        }
+
+#print physics
+
+sampling={
+        'raw_sample_size':160,
+#        'dataShape':(199,4)     # uniform
+#        'dataShape':(188,4)     # face 3
+        'dataShape':(195,4)     # face 2
+#        'dataShape':(195,4)     # face 1
+        }
+
+# data entry parameters
+
+dataEntry={
+        'startTime':5.5,  # KinecticEnergy stationary
+        'endTime':7.2,
+        'chunkStep':50,
+        'NbOfFiles':171,
+        #old path : 
+        #'path':"/store/caseByGeometry/T/new-mesh/pointwise/postProcessing/1d_mapped_NearestFace",
+        #data position :
+        #/store/lmfa/fct/hluo/zaurak/caseByGeometry/T/new-mesh/pointwise/postProcessing/
+        #/home/lmfa/hluo/LocalSoftware/OpenFOAM/hluo-2.3.x/run/T/1d_mapped_NearestFace
+        #'path':'/store/T_c/1d_mapped_NearestFace'
+        'path':database['D1-1d']['targetDir']
+        }
+
+parameters={
+        'physics':physics,
+        'sampling':sampling,
+        'dataEntry':dataEntry,
+        }
+
+plotSavePath="10D_5Dforced/"
+
+# secondary physical parameters
+#Retau=uTau*R/nu
