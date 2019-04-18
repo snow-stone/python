c3b3e040df678f391ff23ef1cf25f42bb2125dcc
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-18 23:11:42 +0200

    update

--- Analysis/Tools/timeSeries_returnOuterVariables/run.sh
+++ Analysis/Tools/timeSeries_returnOuterVariables/run.sh
@@ -1,66 +1,159 @@
 #!/bin/bash
 
 #caseAlias=D1-1b
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 #
 #caseAlias=D1-1d
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 #
 #caseAlias=D1-1d_lR2_afterAugust
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 #
 #caseAlias=D2-NN-1j_test_from0
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ #python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 #
 #caseAlias=D2-NN-1j_test_from0p3_forcingStep_St1_A_eq_0p05
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
 
 #caseAlias=D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ ControlFig_usingParameters
 
 #caseAlias=D1-1j_mapped
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 
 #caseAlias=D2-1j_mapped
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 
 #caseAlias=D3-1j_mapped
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 
 #caseAlias=D2-1j_syn
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 
 #caseAlias=D2-NN-1j_syn
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/
 
-caseAlias=D2-NN-1k_syn
-#python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
-#python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
-#python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#caseAlias=D2-NN-1k_syn
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
 
-caseAlias=D2-NN-1k_syn_forcing
-python cut1_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
-python cut2_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
-python cut3_new_static_argv.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#caseAlias=D2-NN-1k_syn_forcing
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/$caseAlias/ NoControlFig_usingParameters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#caseAlias=D1-1b
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+
+#caseAlias=D1-1d
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+
+#caseAlias=D1-1d_lR2_afterAugust
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+
+#caseAlias=D2-NN-1j_test_from0
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-NN-1j_test_from0p3_forcingStep_St1_A_eq_0p05
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D1-1j_mapped
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-1j_mapped
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D3-1j_mapped
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-1j_syn
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-NN-1j_syn
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-NN-1k_syn
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#
+#caseAlias=D2-NN-1k_syn_forcing
+#python cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+
+
+
+
+
+
+
+
+
+
+
+
+caseAlias=D1-1j_mapped
+#python FIG1_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python FIG1_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python FIG1_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
