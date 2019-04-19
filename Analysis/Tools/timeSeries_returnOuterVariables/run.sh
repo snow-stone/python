3e0aa5762c8b525767331b4e9d0b70efcfd24d8b
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-19 16:57:30 +0200

    update

--- Analysis/Tools/timeSeries_returnOuterVariables/run.sh
+++ Analysis/Tools/timeSeries_returnOuterVariables/run.sh
@@ -154,9 +154,9 @@
 
 
 caseAlias=D1-1j_mapped
-python FIG1_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
-python FIG1_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
-python FIG1_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python FIG1_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python FIG1_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+#python FIG1_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
 
 caseAlias=D2-1j_mapped
 #python FIG2_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
@@ -187,3 +187,40 @@ caseAlias=D2-NN-1k_syn_forcing
 #python FIG7_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
 #python FIG7_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
 #python FIG7_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/LatexTemplate/T_c_draft/PICTURE/chunkPlots/$caseAlias/ NoControlFig_usingParameters
+
+
+
+caseAlias=D1-1j_mapped
+python FIG1_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG1/ NoControlFig_usingParameters
+python FIG1_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG1/ NoControlFig_usingParameters
+python FIG1_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG1/ NoControlFig_usingParameters
+
+caseAlias=D2-1j_mapped
+python FIG2_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG2/ NoControlFig_usingParameters
+python FIG2_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG2/ NoControlFig_usingParameters
+python FIG2_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG2/ NoControlFig_usingParameters
+
+caseAlias=D3-1j_mapped
+python FIG3_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
+python FIG3_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
+python FIG3_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
+
+caseAlias=D2-NN-1j_test_from0
+python FIG4_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG4/ NoControlFig_usingParameters
+python FIG4_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG4/ NoControlFig_usingParameters
+python FIG4_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG4/ NoControlFig_usingParameters
+
+caseAlias=D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05
+python FIG5_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG5/ NoControlFig_usingParameters
+python FIG5_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG5/ NoControlFig_usingParameters
+python FIG5_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG5/ NoControlFig_usingParameters
+
+caseAlias=D2-NN-1k_syn
+python FIG6_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG6/ NoControlFig_usingParameters
+python FIG6_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG6/ NoControlFig_usingParameters
+python FIG6_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG6/ NoControlFig_usingParameters
+
+caseAlias=D2-NN-1k_syn_forcing
+python FIG7_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG7/ NoControlFig_usingParameters
+python FIG7_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG7/ NoControlFig_usingParameters
+python FIG7_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG7/ NoControlFig_usingParameters
