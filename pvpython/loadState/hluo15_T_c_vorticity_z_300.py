d414ca807a4a713093279c6605b3a2d3fcb24d2c
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-04-30 12:38:19 +0200

    update for adding dirName

--- pvpython/loadState/hluo15_T_c_vorticity_z_300.py
+++ pvpython/loadState/hluo15_T_c_vorticity_z_300.py
@@ -17,11 +17,15 @@ renderView1 = GetActiveViewOrCreate('RenderView')
 Delete(renderView1)
 del renderView1
 
+import sys
+
+dirName = sys.argv[1]
+
 # load state
-LoadState('/store/T_c/1j/D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05/hluo15_T_c_vorticity_z_300.pvsm', LoadStateDataFileOptions='Use File Names From State',
-    DataDirectory='/store/T_c/1j/D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05',
+LoadState(dirName+'/'+'hluo15_T_c_vorticity_z_300.pvsm', LoadStateDataFileOptions='Use File Names From State',
+    DataDirectory=dirName,
     OnlyUseFilesInDataDirectory=0,
-    D2NN1k_syn_forcingfoamFileName='/store/T_c/1j/D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05/D2-NN-1k_syn_forcing.foam')
+    D2NN1k_syn_forcingfoamFileName=dirName+'/'+'D2-NN-1k_syn_forcing.foam')
 
 # get active view
 renderView1 = GetActiveViewOrCreate('RenderView')
@@ -35,7 +39,7 @@ renderView1.CameraViewUp = [0.1922771356690952, 0.9426521851791163, -0.272830278
 renderView1.CameraParallelScale = 0.09044328636212606
 
 # save screenshot
-SaveScreenshot('/store/T_c/1j/D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05/hluo15_T_c_vorticity_z_300.png', renderView1, ImageResolution=[2754, 1838],
+SaveScreenshot(dirName+'/'+'hluo15_T_c_vorticity_z_300.png', renderView1, ImageResolution=[2754, 1838],
     FontScaling='Scale fonts proportionally',
     OverrideColorPalette='',
     StereoMode='No change',
@@ -54,3 +58,9 @@ renderView1.CameraParallelScale = 0.09044328636212606
 #### uncomment the following to render all views
 # RenderAllViews()
 # alternatively, if you want to write images, you can use SaveScreenshot(...).
+
+import os
+print "Finalizing "+ os.path.basename(__file__) +" " + "@ dir : " + dirName 
+import datetime
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+print "==============================="
