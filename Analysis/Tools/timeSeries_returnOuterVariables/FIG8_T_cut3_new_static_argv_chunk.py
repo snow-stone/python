8c729d68dbdc67dbac5a9784a667d84e8ce6ea57
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-05-03 15:41:54 +0200

    FIG8 : legend 1 colon , remove extra legend, add image to plot

--- Analysis/Tools/timeSeries_returnOuterVariables/FIG8_T_cut3_new_static_argv_chunk.py
+++ Analysis/Tools/timeSeries_returnOuterVariables/FIG8_T_cut3_new_static_argv_chunk.py
@@ -163,6 +163,13 @@ def main():
     ax2.set_ylabel(r'$rms(c)$')
     #ax2.set_title(alias_dict[parameterFileBasename])
     
+    im = plt.imread('cutPositions_cropped_cut3.png')
+    rect=[0.1, 0.8, 0.3, 0.3]
+    ax1_new = fig1.add_axes(rect, anchor='NE', zorder=-1)
+    ax1_new.imshow(im)
+    ax1_new.axis('off')
+    ax1.get_legend().remove()
+    ax2.get_legend().remove()
     fig1.savefig(saveDir+"T_cut3a.png",  bbox_inches='tight')
     fig2.savefig(saveDir+"T_cut3b.png",  bbox_inches='tight')
 
