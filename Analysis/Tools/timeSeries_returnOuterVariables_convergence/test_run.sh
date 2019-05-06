ac0579ad703cc7ac9cbf8859377635a89ab8a866
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-05-06 08:40:39 +0200

    update : rm convergence/test.py (polynome are not good enough as basis) ; test_run.sh

--- Analysis/Tools/timeSeries_returnOuterVariables_convergence/test_run.sh
+++ Analysis/Tools/timeSeries_returnOuterVariables_convergence/test_run.sh
@@ -16,7 +16,7 @@ caseAlias=D3-1j_mapped
 #python FIG3_cut1_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
 #python FIG3_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
 #python FIG3_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG3/ NoControlFig_usingParameters
-python FIG9_T_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG9/ NoControlFig_usingParameters
+#python FIG9_T_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG9/ NoControlFig_usingParameters
 #python FIG9_T_cut3_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG9/ NoControlFig_usingParameters
 
 #python FIG46_cut1_new_static_argv_chunk.py D2-NN-1j_test_from0 D2-NN-1k_syn /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG46/ NoControlFig_usingParameters
@@ -27,5 +27,5 @@ python FIG9_T_cut2_new_static_argv_chunk.py $caseAlias /home/hluo/work/git/thesi
 #python FIG57_cut2_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG57/ NoControlFig_usingParameters
 #python FIG57_cut3_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG57/ NoControlFig_usingParameters
 
-#python FIG10_T_cut2_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG10/ NoControlFig_usingParameters
-#python FIG10_T_cut3_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG10/ NoControlFig_usingParameters
+python FIG10_T_cut2_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG10/ NoControlFig_usingParameters
+python FIG10_T_cut3_new_static_argv_chunk.py D2-NN-1j_test_from0p3_forcingSinus_St3p2_A_eq_0p05 D2-NN-1k_syn_forcing /home/hluo/work/git/thesis/Thesis_hluo_new/ch3/images/lines/FIG10/ NoControlFig_usingParameters
