ef13abee6ccc9a32e86caf4fb64269fcc1bd27c8
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-10-14 10:12:12 +0200

    copy userHistogramNoLog.py

--- userHistogram/userHistogramNoLogNew_nu.py
+++ userHistogram/userHistogramNoLogNew_nu.py
@@ -3,69 +3,6 @@ matplotlib.use('agg')
 import numpy as np
 import matplotlib.pyplot as plt
 
-def slice_T_mean_hist(dataFileName, marker, path2Data, caseName, alias):
-    plt.style.use('seaborn-white') # from defaut
-    plt.rcParams.update({'font.size': 20})
-    plt.rcParams['savefig.dpi'] = 100
-    
-    rawData = np.genfromtxt(path2Data+"/"+caseName+"/"+dataFileName+".csv", delimiter=',', skip_header=1)
-    
-    T = rawData[:,0]
-    
-    fig, ax = plt.subplots()
-
-#    n, bins, patches = ax.hist(T, bins=40, weights=np.ones(len(T)) / len(T), color='g')    
-    n, bins, patches = ax.hist(T, np.linspace(-0.05,1.05,50), weights=np.ones(len(T)) / len(T), color='darkcyan', normed=False, histtype='stepfilled', alpha=1)
-#    n, bins, patches = ax.hist(T, bins=np.linspace(0,1,40), weights=np.ones(len(T)) / len(T), color='g')
-#    n, bins, patches = ax.hist(T, bins=40, color='g', normed=True)
-    
-#    print "bins :", bins
-#    print "n :", n
-    print "sum(n) : ", sum(n)
-#    print "n*bins :", n*1./50
-#    print "sum :", sum(n*1./50)
-    
-    mean = np.mean(T)
-    rms  = np.std(T)
-    
-#    ymin, ymax = ax.get_ylim()
-#    xmin, xmax = ax.get_xlim()
-    ax.set_xlim(-0.02,1.02)
-#    ax.set_xlim(-0.02,1.02)
-    ax.set_ylim(0,0.4)
-#    xmax=1
-#    ymax=30
-#    ax.text(xmax*0.7, ymax*0.8, r'$\mu=%.2f$'% mean)
-#    ax.text(xmax*0.7, ymax*0.7, r'$\sigma=%.2f$'% rms)
-#    ax.grid(True)
-    ax.tick_params(axis='both', direction='out', length=8, width=3)
-    ax.set_xticklabels([''])
-    ax.set_yticklabels([''])
-    
-#    ax.axvline(x=0.5, color='blue', linewidth=1)
-    ax.axvline(x=mean, color='red', linewidth=3)
-    if (mean-rms) > 0:
-        ax.axvline(x=mean-rms, color='black', linewidth=3, linestyle='--')
-    if (mean+rms) < 1.0:
-        ax.axvline(x=mean+rms, color='black', linewidth=3, linestyle='--')
-    
-#    ax.set_title(r"$@%s$" % marker)
-#    ax.set_xlabel(r'$\overline{T}$')
-#    ax.set_ylabel('The number of cells in '+r'$\%$')
-#    plt.tick_params(
-#    axis='x',          # changes apply to the x-axis
-#    which='both',      # both major and minor ticks are affected
-#    bottom=False,      # ticks along the bottom edge are off
-#    top=False,         # ticks along the top edge are off
-#    labelbottom=False) # labels along the bottom edge are off
-    ax.text(-0.17, 0.35, r'$0.4$', fontsize=30)
-    ax.text(-0.17, 0.15, r'$0.2$', fontsize=30)
-    ax.text(-0.1, -0, r'$0$', fontsize=30)
-    ax.text(0, -0.05, r'$0$', fontsize=30)
-    ax.text(0.45, -0.05, r'$0.5$', fontsize=30)
-    ax.text(1, -0.05, r'$1$', fontsize=30)
-    
-    fig.savefig(path2Data+"/"+caseName+"/"+"hist_"+dataFileName[:-1]+".png",  bbox_inches='tight')
     
 def slice_nu_mean_hist_noLog(dataFileName, marker, path2Data, caseName, alias, ifPlotHist):
     plt.style.use('seaborn-white') # from defaut
