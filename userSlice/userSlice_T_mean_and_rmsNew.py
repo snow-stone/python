64679c20a4f38cc2af857dfed95239c84ca9557e
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-10-17 13:19:39 +0200

    important : change to I_s

--- userSlice/userSlice_T_mean_and_rmsNew.py
+++ userSlice/userSlice_T_mean_and_rmsNew.py
@@ -110,6 +110,17 @@ def Figure_ref_local(caseList, path2Data, saveFigDir):
             
         positionList = np.asarray(positionList)
         ax_cases.plot(positionList/8.0, meanOfRMS/meanOfMEAN, label=aliasDict[dataDir], marker=markerDict[dataDir], markersize=8, color=colorDict[dataDir], linestyle=linestyleDict[dataDir], linewidth=linewidthDict[dataDir], markeredgecolor=colorDict[dataDir], markerfacecolor=markerfacecolorDict[dataDir], markeredgewidth=1.5)
+
+    def plotCaseWithSlices_local_MI_2(ax_cases, path2Data, dataDir, positionList, aliasDict, markerDict, colorDict, linestyleDict, linewidthDict, markerfacecolorDict, cut, ifPlotInter):
+        meanOfMEAN = np.zeros(len(positionList))
+        meanOfRMS = np.zeros(len(positionList))
+        
+        fig, ax_in_case = plt.subplots()
+        for i, position in enumerate(positionList):
+            meanOfMEAN[i], meanOfRMS[i] = plotSlice_local(ax_in_case, position, path2Data, dataDir, cut, ifPlotInter)
+            
+        positionList = np.asarray(positionList)
+        ax_cases.plot(positionList/8.0, meanOfRMS/meanOfRMS[0], label=aliasDict[dataDir], marker=markerDict[dataDir], markersize=8, color=colorDict[dataDir], linestyle=linestyleDict[dataDir], linewidth=linewidthDict[dataDir], markeredgecolor=colorDict[dataDir], markerfacecolor=markerfacecolorDict[dataDir], markeredgewidth=1.5)
     
     print "README :"
     print "This is the version where we take slice averaged value as reference"
@@ -242,12 +253,14 @@ def Figure_ref_local(caseList, path2Data, saveFigDir):
     for i, caseDir in enumerate(caseList):
         print "caseDir : ", caseDir
 #        plotCaseWithSlices_local_MI(axes[2], path2Data, caseDir, positionList, aliasDict, markerDict, colorDict, cut=0.7, ifPlotInter=False)
-        plotCaseWithSlices_local_MI_1(axes[2], path2Data, caseDir, positionList, aliasDict, markerDict, colorDict, linestyleDict, linewidthDict, markerfacecolorDict, cut=0.7, ifPlotInter=False)
+        #plotCaseWithSlices_local_MI_1(axes[2], path2Data, caseDir, positionList, aliasDict, markerDict, colorDict, linestyleDict, linewidthDict, markerfacecolorDict, cut=0.7, ifPlotInter=False)
+        plotCaseWithSlices_local_MI_2(axes[2], path2Data, caseDir, positionList, aliasDict, markerDict, colorDict, linestyleDict, linewidthDict, markerfacecolorDict, cut=0.7, ifPlotInter=False)
         
     axes[2].set_xlabel(r"$x/D$")
 #    axes[2].set_ylabel(r"$\frac{[<T-T_{ref}>_{(y,z)}^2]^{1/2}}{[<T-T_{ref}>_{(y,z)}^2]^{1/2}|_{x=0}}$")
 #    axes[2].set_ylabel(r"$\frac{[<T-T_{ref}>_{(y,z)}^2]^{1/2}}{<T>_{y,z}}$")
-    axes[2].set_ylabel(r"$\sigma_{\overline{c}} / \mu_{\overline{c}}$")
+    #axes[2].set_ylabel(r"$\sigma_{\overline{c}} / \mu_{\overline{c}}$")
+    axes[2].set_ylabel(r"$I_s$")
 #    axes[2].legend(bbox_to_anchor=(1, 1), ncol=2, shadow=True)
 #    ax_principle2.set_ylim(0,1.0)
 #    fig.savefig(path2Data+"/"+saveFigDir+'/mixing.png', bbox_inches='tight')
